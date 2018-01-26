b6b5ce69f509a0c12e6941ac81fc393049196c46
Andrew Mastbaum <mastbaum@uchicago.edu>
2018-01-26 11:51:18 -0600

    add bin to PATH

--- sbnanalysis/env.sh
+++ sbnanalysis/env.sh
@@ -3,4 +3,5 @@
 export SBN_ANALYSIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export SBN_LIB_DIR=$SBN_ANALYSIS_DIR/build/lib
 export LD_LIBRARY_PATH=$SBN_LIB_DIR:$LD_LIBRARY_PATH
+export PATH=$SBN_ANALYSIS_DIR/build/bin:$PATH
 
