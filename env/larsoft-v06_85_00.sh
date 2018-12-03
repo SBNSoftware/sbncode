cf27797fcc2959da255b2e6e87630c02dcb10ead
Andrew Mastbaum <mastbaum@uchicago.edu>
2018-12-03 14:40:15 -0600

    update version to v07_11_00

--- env/larsoft-v06_85_00.sh
+++
@@ -1,8 +0,0 @@
-LAR_VERSION="v06_85_00"
-LAR_QUAL="e15:prof"
-source /cvmfs/uboone.opensciencegrid.org/products/setup_uboone.sh
-setup uboonecode ${LAR_VERSION} -q ${LAR_QUAL}
-source /cvmfs/sbnd.opensciencegrid.org/products/sbnd/setup_sbnd.sh
-setup sbndcode ${LAR_VERSION} -q ${LAR_QUAL}
-source /cvmfs/icarus.opensciencegrid.org/products/icarus/setup_icarus.sh
-setup icaruscode ${LAR_VERSION} -q ${LAR_QUAL}
