6ffa3a867f77000801e87daa3518e17c5df94fac
Rhiannon Jones <rjones0211@hotmail.com>
2018-12-05 13:41:15 -0600

    Getting things in order, SelectionTool, SelectionObjects and SelectionAnalysis exist

---
+++ sbnanalysis/ana/SelectionAnalysis/TestDuringMerge.h
@@ -0,0 +1,86 @@
+#ifndef __sbnanalysis_ana_SelectionAnalysis_TestDuringMerge__
+#define __sbnanalysis_ana_SelectionAnalysis_TestDuringMerge__
+
+/**
+ * \file TestDuringMerge.h
+ *
+ * An example event selection processor.
+ *
+ * This is an implementation of the core::SelectionBase class. We define
+ * the methods called for initialization, finalization, and event-by-event
+ * processing.
+ *
+ * Author: A. Mastbaum <mastbaum@uchicago.edu>
+ */
+
+// Includes
+#include <iostream>
+#include "canvas/Utilities/InputTag.h"
+#include "core/SelectionBase.hh"
+#include "core/Event.hh"
+
+// Forward declarations
+class TH2D;
+
+/** All analysis code is defined in namespace "ana" */
+namespace ana {
+
+  /** Code specific to the SelectionAnalysis. */
+  namespace SelectionAnalysis {
+
+/**
+ * \class TestDuringMerge
+ * \brief An example selection analysis
+ *
+ * This selection analysis doesn't actually select events, it just
+ * demonstrates the framework!
+ */
+class TestDuringMerge : public core::SelectionBase {
+public:
+  /** Constructor. */
+  TestDuringMerge();
+
+  /**
+   * Initialization.
+   *
+   * Here we load configuration parameters, set up histograms for output, and
+   * add our own branches to the output tree.
+   *
+   * \param config A configuration, as a JSON object
+   */
+  void Initialize(Json::Value* config=NULL);
+
+  /** Finalize and write objects to the output file. */
+  void Finalize();
+
+  /**
+   * Process one event.
+   *
+   * \param ev A single event, as a gallery::Event
+   * \param reco Reconstructed interactions
+   * \return True to keep event
+   */
+  bool ProcessEvent(const gallery::Event& ev,
+                    const std::vector<Event::Interaction>& truth,
+                    std::vector<Event::RecoInteraction>& reco);
+
+protected:
+  unsigned fEventCounter;  //!< Count processed events
+
+  /** Configuration parameters */
+  art::InputTag fTruthTag;  //!< art tag for MCTruth information
+  int fMyParam;  //!< A parameter from the configuration file
+
+  /** Custom data branches */
+  int fNuCount;  //!< Number of neutrino interactions in the event
+  int fMyVar;  //!< Another variable of interest
+
+  /** Histograms */
+  TH2D* fNuVertexXZHist;  //!< Neutrino vertex XZ projection
+};
+
+  }  // namespace SelectionAnalysis
+}  // namespace ana
+
+#endif  // __sbnanalysis_ana_SelectionAnalysis_TestDuringMerge__
+
--- sbnanalysis/ana/SelectionTool/ObjectBuilder.cxx
+++ sbnanalysis/ana/SelectionTool/ObjectBuilder.cxx
@@ -11,43 +11,33 @@
 namespace ana {
   namespace SelectionTool {
 
-    ObjectBuilder::ObjectBuilder() : SelectionBase() {}
-
+    ObjectBuilder::ObjectBuilder() {}
 
     //void ObjectBuilder::Initialize(Json::Value* config) {
     void ObjectBuilder::Initialize(fhicl::ParameterSet const &p) {
       // Initialise the objects we are accessing
 
       // Handle labels                                                                
-      fGeneratorLabel;            = p.get<art::InputTag>("TruthLabel");              
-      fGeantLabel;                = p.get<art::InputTag>("G4Label");                 
-      fPandoraLabel;              = p.get<art::InputTag>("PandoraLabel");            
-      fRecoTrackLabel;            = p.get<art::InputTag>("RecoTrackLabel");          
-      fRecoShowerLabel;           = p.get<art::InputTag>("RecoShowerLabel");         
-      fRecoTrackCalorimetryLabel; = p.get<art::InputTag>("RecoTrackCalorimetryLabel");
-      fRecoTrackParticleidLabel;  = p.get<art::InputTag>("RecoTrackParticleIDLabel");
-      fHitLabel;                  = p.get<art::InputTag>("HitLabel");                
+      fGeneratorLabel            = p.get<art::InputTag>("TruthLabel");              
+      fGeantLabel                = p.get<art::InputTag>("G4Label");                 
+      fPandoraLabel              = p.get<art::InputTag>("PandoraLabel");            
+      fRecoTrackLabel            = p.get<art::InputTag>("RecoTrackLabel");          
+      fRecoShowerLabel           = p.get<art::InputTag>("RecoShowerLabel");         
+      fRecoTrackCalorimetryLabel = p.get<art::InputTag>("RecoTrackCalorimetryLabel");
+      fRecoTrackParticleidLabel  = p.get<art::InputTag>("RecoTrackParticleIDLabel");
+      fHitLabel                  = p.get<art::InputTag>("HitLabel");                
     }
-
 
     void ObjectBuilder::Finalize() {
       // Output our histograms to the ROOT file
       //fOutputFile->cd();
     }
 
-
     bool ObjectBuilder::ProcessEvent(const gallery::Event& ev, std::vector<Event::RecoInteraction>& reco) {
-
-      // Example of using library data
-      ana::ExampleAnalysis::hello();
-
       return true;
     } // ProcessEvent function
-
   }  // namespace SelectionTool
 }  // namespace ana
 
 
-// This line must be included for all selections!
-DECLARE_SBN_PROCESSOR(ana::SelectionTool::ObjectBuilder)
 
