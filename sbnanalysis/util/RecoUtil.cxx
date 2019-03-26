5fec6dd6dcaa0509b88a30077013ac340113ac94
Rhiannon Jones <rjones0211@hotmail.com>
2019-03-26 18:51:06 -0500

    Added to RecoUtil the ability to check if a position is contained within the detector volume. Tested geometry given by LArSoft: Our 2 TPCs are the same size but are both the size of the 2 TPCs added together

--- sbnanalysis/util/RecoUtil.cxx
+++ sbnanalysis/util/RecoUtil.cxx
@@ -101,7 +101,56 @@ namespace util {
     }
     return objectTrack;
   } // TrueParticleIDFromTotalRecoCharge
+  
+  bool IsInsideTPC(TVector3 position, core::ProviderManager* provider_manager, double distance_buffer){
+    double vtx[3] = {position.X(), position.Y(), position.Z()};
+    bool inside = false;
+    const geo::GeometryCore* geom = provider_manager->GetGeometryProvider();
+    geo::TPCID idtpc = geom->FindTPCAtPosition(vtx);
 
+    if (geom->HasTPC(idtpc))
+    {
+      const geo::TPCGeo& tpcgeo = geom->GetElement(idtpc);
+      double minx = tpcgeo.MinX(); double maxx = tpcgeo.MaxX();
+      double miny = tpcgeo.MinY(); double maxy = tpcgeo.MaxY();
+      double minz = tpcgeo.MinZ(); double maxz = tpcgeo.MaxZ();
+
+      for (size_t c = 0; c < geom->Ncryostats(); c++)
+      {
+        const geo::CryostatGeo& cryostat = geom->Cryostat(c);
+        for (size_t t = 0; t < cryostat.NTPC(); t++)
+        {
+          const geo::TPCGeo& tpcg = cryostat.TPC(t);
+          if (tpcg.MinX() < minx) minx = tpcg.MinX();
+          if (tpcg.MaxX() > maxx) maxx = tpcg.MaxX();
+          if (tpcg.MinY() < miny) miny = tpcg.MinY();
+          if (tpcg.MaxY() > maxy) maxy = tpcg.MaxY();
+          if (tpcg.MinZ() < minz) minz = tpcg.MinZ();
+          if (tpcg.MaxZ() > maxz) maxz = tpcg.MaxZ();
+        }
+      }
+
+      //x
+      double dista = fabs(minx - position.X());
+      double distb = fabs(position.X() - maxx);
+      if ((position.X() > minx) && (position.X() < maxx) &&
+          (dista > distance_buffer) && (distb > distance_buffer)) inside = true;
+      //y
+      dista = fabs(maxy - position.Y());
+      distb = fabs(position.Y() - miny);
+      if (inside && (position.Y() > miny) && (position.Y() < maxy) &&
+          (dista > distance_buffer) && (distb > distance_buffer)) inside = true;
+      else inside = false;
+      //z
+      dista = fabs(maxz - position.Z());
+      distb = fabs(position.Z() - minz);
+      if (inside && (position.Z() > minz) && (position.Z() < maxz) &&
+          (dista > distance_buffer) && (distb > distance_buffer)) inside = true;
+      else inside = false;
+    }
+
+    return inside;
+  } // IsInsideTPC
 
 } // namespace util
 
