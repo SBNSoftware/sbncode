97f394a8a69b2b9c76cfcbceca9ba15bfe55e916
Dominic Barker <dominic.barker@sheffield.ac.uk>
2019-04-02 05:43:47 -0500

    pulling

---
+++ sbnutil/setup_sbncode_grid.sh
@@ -0,0 +1,26 @@
+
+source /cvmfs/uboone.opensciencegrid.org/products/setup_uboone.sh
+echo "setting up uboonecode version:        v08_12_00"
+setup uboonecode        v08_12_00 -q e17:prof
+unsetup larbatch
+
+source /cvmfs/sbnd.opensciencegrid.org/products/sbnd/setup_sbnd.sh    
+echo "setting up sbndcode version:          v08_12_00"
+setup sbndcode          v08_12_00 -q e17:prof
+unsetup larbatch
+
+source /cvmfs/icarus.opensciencegrid.org/products/icarus/setup_icarus.sh
+echo "setting up icaruscode version:        v08_12_00"
+setup icaruscode         v08_12_00 -q e17:prof 
+
+if [ "sbnd"  == "sbnd" ]; then
+    source /cvmfs/sbnd.opensciencegrid.org/products/sbnd/setup_sbnd.sh
+elif [ "sbnd"  == "uboone" ]; then
+    source /cvmfs/uboone.opensciencegrid.org/products/setup_uboone.sh
+elif [ "sbnd"  == "icarus" ]; then
+    source /cvmfs/icarus.opensciencegrid.org/products/icarus/setup_icarus.sh
+else
+    echo "Sorry. Are you not on a gpvm?"
+    exit
+fi
+
