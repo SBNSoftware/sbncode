06172a53c53004ee9a0f2dad0403db3dd508b69d
Christopher Backhouse <bckhouse@fnal.gov>
2019-05-14 07:48:05 -0500

    Hook things up. Makes a file that looks good.

--- sbnanalysis/FlatMaker/convert_to_flat.C
+++ sbnanalysis/FlatMaker/convert_to_flat.C
@@ -8,36 +8,33 @@
 
 void convert_to_flat()
 {
-  //  gSystem->Load("srcs/sbncode/sbnanalysis/build/core/libsbnanalysis_Event.so");
+  TFile* fin = TFile::Open("/pnfs/sbnd/persistent/users/gputnam/numu_simulation_12_05_2018/processed/output_SBNOsc_NumuSelection_Modern_SBND.root");
 
-  //  TFile* f = new TFile("/sbnd/data/users/bckhouse/processed_1.tempwgh/output_SBNOsc_NumuSelection_Modern_SBND.root");
-
-  TFile* f = new TFile("/pnfs/sbnd/persistent/users/gputnam/numu_simulation_12_05_2018/processed/output_SBNOsc_NumuSelection_Modern_SBND.root");
-
-  TTree* tr = (TTree*)f->Get("sbnana");
+  TTree* tr = (TTree*)fin->Get("sbnana");
 
   Event* event = 0;
   tr->SetBranchAddress("events", &event);
 
-  //  TH1* hwei = new TH1F("", "", 100, 0, 2);
+  TFile fout("flat.root", "RECREATE");
+
+  TTree* trout = new TTree("rec", "rec");
+  // Have trouble with memory usage (because several trees are open at
+  // once?). Set the maximum buffer usage (per tree) to 3MB (10x less than
+  // default)
+  trout->SetAutoFlush(-3*1000*1000);
+
+  flat::FlatEvent* rec = new flat::FlatEvent("rec.", trout, 0);//policy);
 
   for(int i = 0; i < tr->GetEntries(); ++i){
-    std::cout << i << " / " << tr->GetEntries() << std::endl;
+    // TODO use CAFAna progress bar
+    if(i%1000 == 0) std::cout << i/1000 << " / " << tr->GetEntries()/1000 << std::endl;
 
     tr->GetEntry(i);
 
-    // //    std::cout << event->truth.size() << std::endl;
-    // if(!event->truth.empty()){
-    //   //      std::cout << event->truth[0].neutrino.pdg << std::endl;
-    //   double weiUniv0 = 1;
-    //   for(auto it: event->truth[0].weights){
-    //     weiUniv0 *= it.second[0];
-    //     //        std::cout << it.first << " " << it.second.size() << " " << it.second[0] << std::endl;
-    //   }
-    //   //      std::cout << weiUniv0 << std::endl;
-    //   hwei->Fill(weiUniv0);
-    // }
+    rec->Fill(*event);
+    trout->Fill();
   }
 
-  //  hwei->Draw();
+  trout->Write();
+  delete rec;
 }
