41b17057905f2546db5e55b66becf86311e35b79
Christopher Backhouse <bckhouse@fnal.gov>
2019-09-05 08:01:03 -0500

    Some work on the tutorial macros for next week.

--- sbncode/CAFAna/tute/demo0.C
+++ sbncode/CAFAna/tute/demo0.C
@@ -4,68 +4,45 @@
 #include "CAFAna/Core/SpectrumLoader.h"
 #include "CAFAna/Core/Spectrum.h"
 #include "CAFAna/Core/Binning.h"
-#include "CAFAna/Core/Var.h"
 
-// #include "CAFAna/Cuts/TruthCuts.h"
-
-#include "StandardRecord/Proxy/SRProxy.h"
-
-#include "TCanvas.h"
-#include "TH1.h"
-#include "TPad.h"
+#include "CAFAna/Analysis/ExpInfo.h" // for kPOTnominal
 
 using namespace ana;
 
+#include "StandardRecord/Proxy/SRProxy.h"
+
+#include "TH1.h"
+
 void demo0()
 {
-  // Environment variables and wildcards work. As do SAM datasets.
-  //  const std::string fname = "/sbnd/app/users/bzamoran/sbncode-v07_11_00/output_largesample_nu_ExampleAnalysis_ExampleSelection.root";
-
-  const std::string fname = "/pnfs/sbnd/persistent/users/gputnam/numu_simulation_reweight/processed_2.a/output_SBNOsc_NumuSelection_Modern_SBND.root";
+  // Converted from the original sbnanalysis files here
+  // /sbnd/data/users/gputnam/NuMu/outputs/sample_2.1_fitters/
+  //
+  // Environment variables and wildcards work as arguments to
+  // SpectrumLoader. As do SAM datasets.
+  const std::string fname = "/sbnd/data/users/bckhouse/sample_2.1_fitters/output_SBNOsc_NumuSelection_Proposal_SBND.flat.root";
 
   // Source of events
   SpectrumLoader loader(fname);
 
-  // A Var is a little snippet of code that takes a record representing the
-  // event record and returns a single number to plot.
+  // A Var is a little snippet of code that takes a "proxy" representing the
+  // event record and returns a single number to plot
   const Var kTruthEnergy([](const caf::SRProxy* sr)
                          {
                            return sr->truth[0].neutrino.energy;
                          });
 
-  // For such a simple variable you can use a shortcut like this
-  const Var kTruthY = SIMPLEVAR(truth[0].neutrino.inelasticityY);
-
   // Define a spectrum, ie a histogram with associated POT information
   const Binning binsEnergy = Binning::Simple(50, 0, 5);
   const HistAxis axEnergy("True energy (GeV)", binsEnergy, kTruthEnergy);
-  // kIsNumuCC here is a "Cut". Same as a Var but returning a boolean. In this
-  // case, we're only keeping events that are truly numu CC interactions.
-  const Var kIsCC = SIMPLEVAR(truth[0].neutrino.iscc);
-  Spectrum sEnergy(loader, axEnergy, kIsCC != 0);
+  Spectrum sEnergy(loader, axEnergy, kNoCut);
 
-  Spectrum sEnergyNC(loader, axEnergy, kIsCC == 0);
-
-  // And another
-  const Binning binsY = Binning::Simple(50, 0, 1);
-  const HistAxis axY("Y", binsY, kTruthY);
-  Spectrum sY(loader, axY, kIsCC != 0);
-  Spectrum sYNC(loader, axY, kIsCC == 0);
-
-  // This is the call that actually fills in those spectra
+  // This is the call that actually fills in the spectrum
   loader.Go();
 
-  // Fake POT: we need to sort this out in the files first
-  const double pot = 6.e20;
+  // From Analysis/ExpInfo.h
+  const double pot = kPOTnominal; // currently 6.6e20
 
-  // For plotting purposes we can convert to TH1s
-  TCanvas* c1 = new TCanvas("c1");
+  // For plotting purposes we can convert to a TH1
   sEnergy.ToTH1(pot)->Draw("hist");
-  sEnergyNC.ToTH1(pot, kBlue)->Draw("hist same");
-  c1->SaveAs("demo0_plot1.pdf");
-
-  sY.ToTH1(pot)->Draw("hist");
-  sYNC.ToTH1(pot, kBlue)->Draw("hist same");
-  gPad->SetLogy();
-  c1->SaveAs("demo0_plot2.pdf");
 }
