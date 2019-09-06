2b0c8510a4d15a6878312302b89fd15b01bc46bb
gputnam <gputnam@sbndgpvm01.fnal.gov>
2019-09-06 17:06:58 -0500

    Make sure updates to G4 processing still work for cosmic MC. Fix build errors on merge.

--- sbnanalysis/core/ProcessorBase.cxx
+++ sbnanalysis/core/ProcessorBase.cxx
@@ -244,7 +244,7 @@ void ProcessorBase::BuildEventTree(gallery::Event& ev) {
 
   // Get MC truth information
   gallery::Handle<std::vector<simb::MCTruth>> mctruths_handle;
-  bool mctruth_is_valid = ev.getByLabel(fTruthTag, mctruth_handle);
+  bool mctruth_is_valid = ev.getByLabel(fTruthTag, mctruths_handle);
 
   gallery::Handle<std::vector<simb::MCParticle> > mcparticle_list;
   ev.getByLabel(fMCParticleTag, mcparticle_list);
@@ -254,13 +254,9 @@ void ProcessorBase::BuildEventTree(gallery::Event& ev) {
   ev.getByLabel(fTruthTag, gtruths_handle);
   bool genie_truth_is_valid = gtruths_handle.isValid();
 
-  // get associations from MCTruth information to truth 
-  art::FindManyP<simb::MCParticle, sim::GeneratedParticleInfo> truth_to_particles(mctruths_handle, ev, fMCParticleTag);
-
   // Get MCFlux information
   auto const& mcfluxes = \
     *ev.getValidHandle<std::vector<simb::MCFlux> >(fTruthTag);
-  assert(mctruths.size() == mcfluxes.size());
 
   // Get MCEventWeight information
   std::vector<gallery::Handle<std::vector<evwgh::MCEventWeight> > > wghs;
@@ -271,7 +267,7 @@ void ProcessorBase::BuildEventTree(gallery::Event& ev) {
       bool hasWeights = ev.getByLabel(weightTag, this_wgh);
       // coherence check
       if (hasWeights && mctruth_is_valid) {
-        assert(this_wgh->size() == mctruth_handle->size());
+        assert(this_wgh->size() == mctruths_handle->size());
       }
       // store the weights
       wghs.push_back(this_wgh);
@@ -283,7 +279,7 @@ void ProcessorBase::BuildEventTree(gallery::Event& ev) {
   bool flux_is_valid = ev.getByLabel(fFluxTag, mcflux_handle);
   // Check MCFlux information
   if (flux_is_valid && mctruth_is_valid) {
-    assert(mctruth_handle->size() == mcflux_handle->size());
+    assert(mctruths_handle->size() == mcflux_handle->size());
   }
 
 
@@ -297,186 +293,191 @@ void ProcessorBase::BuildEventTree(gallery::Event& ev) {
   fEvent->metadata.subrun = evaux.subRun();
   fEvent->metadata.eventID = evaux.event();
 
-  unsigned n_truth = mctruth_is_valid ? mctruth_handle->size() : 0;
-  for (size_t i=0; i<n_truth; i++) {
-    event::Interaction interaction;
-    interaction.index = i;
+  if (mctruth_is_valid) {
+    // get associations from MCTruth information to truth 
+    art::FindManyP<simb::MCParticle, sim::GeneratedParticleInfo> truth_to_particles(mctruths_handle, ev, fMCParticleTag);
+    unsigned n_truth = mctruths_handle->size();
+    for (size_t i=0; i<n_truth; i++) {
 
-    auto const& mctruth = mctruth_handle->at(i);
+      event::Interaction interaction;
+      interaction.index = i;
 
-    // TODO: What to do with cosmic MC?
-    // For now, ignore them
-    if (!mctruth.NeutrinoSet()) continue;
+      auto const& mctruth = mctruths_handle->at(i);
 
-    // Combine Weights
-    if (!wghs.empty()) {
-      size_t wgh_idx = 0;
-      // Loop through weight generators, which have a list of weights per truth
-      //
-      // NOTE: The code allows for multiple different weight generators to produce weights with the same name.
-      //       What will happen is that the same-named weights from differetn producers will be placed in the same
-      //       vector in the "weights" map below. The order in which weights from different producers are combined
-      //       should be consistent from event to event so that correlations are preserved between events. This 
-      //       is ensured in the current implementation by making sure that the different weight tags in "wghs"
-      //       are always ordered in the same way.
-      for (auto const& wgh : wghs) {
-        for (const std::pair<std::string, std::vector<double> >& this_wgh : wgh->at(i).fWeight) {
-          // If we haven't seen this name before, make a new vector with the name
-          if (interaction.weights.count(this_wgh.first) == 0) {
-            interaction.weights.insert({
-              this_wgh.first,
-              std::vector<float>(this_wgh.second.begin(), this_wgh.second.end())
-            });
+      // TODO: What to do with cosmic MC?
+      // For now, ignore them
+      if (!mctruth.NeutrinoSet()) continue;
+
+      // Combine Weights
+      if (!wghs.empty()) {
+        size_t wgh_idx = 0;
+        // Loop through weight generators, which have a list of weights per truth
+        //
+        // NOTE: The code allows for multiple different weight generators to produce weights with the same name.
+        //       What will happen is that the same-named weights from differetn producers will be placed in the same
+        //       vector in the "weights" map below. The order in which weights from different producers are combined
+        //       should be consistent from event to event so that correlations are preserved between events. This 
+        //       is ensured in the current implementation by making sure that the different weight tags in "wghs"
+        //       are always ordered in the same way.
+        for (auto const& wgh : wghs) {
+          for (const std::pair<std::string, std::vector<double> >& this_wgh : wgh->at(i).fWeight) {
+            // If we haven't seen this name before, make a new vector with the name
+            if (interaction.weights.count(this_wgh.first) == 0) {
+              interaction.weights.insert({
+                this_wgh.first,
+                std::vector<float>(this_wgh.second.begin(), this_wgh.second.end())
+              });
+            }
+            // If we have seen the name before, append this instance to the last one
+            else {
+              interaction.weights.at(this_wgh.first).insert(
+                interaction.weights.at(this_wgh.first).end(),
+                this_wgh.second.begin(),
+                this_wgh.second.end());
+            }
           }
-          // If we have seen the name before, append this instance to the last one
+        }
+      }
+
+      if (mcflux_handle.isValid()) {
+        const simb::MCFlux& flux = mcflux_handle->at(i);
+        interaction.neutrino.parentPDG = flux.fptype;
+        interaction.neutrino.parentDecayMode = flux.fndecay;
+        interaction.neutrino.parentDecayVtx = \
+          TVector3(flux.fvx, flux.fvy, flux.fvz);
+        interaction.neutrino.baseline = flux.fdk2gen + flux.fgen2vtx;
+        interaction.neutrino.initpdg = flux.fntype;
+      }
+
+      TLorentzVector q_labframe;
+
+      // get the list of MCParticles to be considered for this interaction
+      std::vector<art::Ptr<simb::MCParticle>> this_mcparticle_list; 
+      std::vector<const sim::GeneratedParticleInfo *> this_mcparticle_assns;
+      if (mcparticles_is_valid) {
+        this_mcparticle_list = truth_to_particles.at(i);
+        this_mcparticle_assns = truth_to_particles.data(i);
+      }
+
+      if (mctruth.NeutrinoSet()) {
+        // Neutrino
+        const simb::MCNeutrino& nu = mctruth.GetNeutrino();
+        interaction.neutrino.isnc =   nu.CCNC()  && (nu.Mode() != simb::kWeakMix);
+        interaction.neutrino.iscc = (!nu.CCNC()) && (nu.Mode() != simb::kWeakMix);
+        interaction.neutrino.pdg = nu.Nu().PdgCode();
+        interaction.neutrino.targetPDG = nu.Target();
+        interaction.neutrino.genie_intcode = nu.Mode();
+        interaction.neutrino.bjorkenX = nu.X();
+        interaction.neutrino.inelasticityY = nu.Y();
+        interaction.neutrino.Q2 = nu.QSqr();
+        interaction.neutrino.w = nu.W();
+        interaction.neutrino.energy = nu.Nu().EndMomentum().Energy();
+        interaction.neutrino.momentum = nu.Nu().EndMomentum().Vect();
+        interaction.neutrino.position = nu.Nu().Position().Vect();
+
+        // Primary lepton
+        const simb::MCParticle& lepton = nu.Lepton();
+        interaction.lepton.pdg = lepton.PdgCode();
+        interaction.lepton.energy = lepton.Momentum(0).Energy();
+        interaction.lepton.momentum = lepton.Momentum(0).Vect();
+        interaction.lepton.start = lepton.Position(0).Vect();
+        interaction.lepton.status_code = lepton.StatusCode();
+        interaction.lepton.is_primary = (lepton.Process() == "primary");
+
+        // match the MCTruth particle to the MCParticle list -- only the list has trajectory information
+        const simb::MCParticle* lepton_traj = NULL;
+        for (int iparticle = 0; iparticle < this_mcparticle_list.size(); iparticle++) {
+          if (this_mcparticle_assns[iparticle]->hasGeneratedParticleIndex() &&
+              this_mcparticle_assns[iparticle]->generatedParticleIndex() < mctruth.NParticles() && // TODO: why is this number sometimes bigger than the number of particles?
+              mctruth.GetParticle(this_mcparticle_assns[iparticle]->generatedParticleIndex()).TrackId() == lepton.TrackId()) {
+
+            // if a genie particle re-scatters in g4 and makes more particles, then multiple g4 particles can match to a 
+            // genie particle. Thus, we also check that the start location of the associated genie particle matches the g4
+            // and that the pdgid matches (to be on the safe side)
+            const simb::MCParticle& matched_genie_particle = mctruth.GetParticle(this_mcparticle_assns[iparticle]->generatedParticleIndex());
+            if ((matched_genie_particle.Position().Vect() - this_mcparticle_list[iparticle]->Position().Vect()).Mag() < 1e-4 &&
+                matched_genie_particle.PdgCode() == this_mcparticle_list[iparticle]->PdgCode()) {
+
+              lepton_traj = this_mcparticle_list[iparticle].get();
+              // this should only be true for one particle
+              assert(lepton_traj != NULL);
+            }
+          }
+        }
+
+        // TODO: why aren't there matches sometimes?
+        if (lepton_traj != NULL) {
+          interaction.lepton.length = util::MCParticleLength(*lepton_traj);
+          if (fProviderManager != NULL) {
+            interaction.lepton.contained_length = util::MCParticleContainedLength(*lepton_traj, fActiveVolumes);
+          }
           else {
-            interaction.weights.at(this_wgh.first).insert(
-              interaction.weights.at(this_wgh.first).end(),
-              this_wgh.second.begin(),
-              this_wgh.second.end());
+            interaction.lepton.contained_length = event::kUnfilled;
           }
-        }
-      }
-    }
-
-    if (mcflux_handle.isValid()) {
-      const simb::MCFlux& flux = mcflux_handle->at(i);
-      interaction.neutrino.parentPDG = flux.fptype;
-      interaction.neutrino.parentDecayMode = flux.fndecay;
-      interaction.neutrino.parentDecayVtx = \
-        TVector3(flux.fvx, flux.fvy, flux.fvz);
-      interaction.neutrino.baseline = flux.fdk2gen + flux.fgen2vtx;
-      interaction.neutrino.initpdg = flux.fntype;
-    }
-
-    TLorentzVector q_labframe;
-
-    // get the list of MCParticles to be considered for this interaction
-    std::vector<art::Ptr<simb::MCParticle>> this_mcparticle_list; 
-    std::vector<const sim::GeneratedParticleInfo *> this_mcparticle_assns;
-    if (mcparticles_is_valid) {
-      this_mcparticle_list = truth_to_particles.at(i);
-      this_mcparticle_assns = truth_to_particles.data(i);
-    }
-
-    if (mctruth.NeutrinoSet()) {
-      // Neutrino
-      const simb::MCNeutrino& nu = mctruth.GetNeutrino();
-      interaction.neutrino.isnc =   nu.CCNC()  && (nu.Mode() != simb::kWeakMix);
-      interaction.neutrino.iscc = (!nu.CCNC()) && (nu.Mode() != simb::kWeakMix);
-      interaction.neutrino.pdg = nu.Nu().PdgCode();
-      interaction.neutrino.targetPDG = nu.Target();
-      interaction.neutrino.genie_intcode = nu.Mode();
-      interaction.neutrino.bjorkenX = nu.X();
-      interaction.neutrino.inelasticityY = nu.Y();
-      interaction.neutrino.Q2 = nu.QSqr();
-      interaction.neutrino.w = nu.W();
-      interaction.neutrino.energy = nu.Nu().EndMomentum().Energy();
-      interaction.neutrino.momentum = nu.Nu().EndMomentum().Vect();
-      interaction.neutrino.position = nu.Nu().Position().Vect();
-
-      // Primary lepton
-      const simb::MCParticle& lepton = nu.Lepton();
-      interaction.lepton.pdg = lepton.PdgCode();
-      interaction.lepton.energy = lepton.Momentum(0).Energy();
-      interaction.lepton.momentum = lepton.Momentum(0).Vect();
-      interaction.lepton.start = lepton.Position(0).Vect();
-      interaction.lepton.status_code = lepton.StatusCode();
-      interaction.lepton.is_primary = (lepton.Process() == "primary");
-
-      // match the MCTruth particle to the MCParticle list -- only the list has trajectory information
-      const simb::MCParticle* lepton_traj = NULL;
-      for (int iparticle = 0; iparticle < this_mcparticle_list.size(); iparticle++) {
-        if (this_mcparticle_assns[iparticle]->hasGeneratedParticleIndex() &&
-            this_mcparticle_assns[iparticle]->generatedParticleIndex() < mctruth.NParticles() && // TODO: why is this number sometimes bigger than the number of particles?
-            mctruth.GetParticle(this_mcparticle_assns[iparticle]->generatedParticleIndex()).TrackId() == lepton.TrackId()) {
-
-          // if a genie particle re-scatters in g4 and makes more particles, then multiple g4 particles can match to a 
-          // genie particle. Thus, we also check that the start location of the associated genie particle matches the g4
-          // and that the pdgid matches (to be on the safe side)
-          const simb::MCParticle& matched_genie_particle = mctruth.GetParticle(this_mcparticle_assns[iparticle]->generatedParticleIndex());
-          if ((matched_genie_particle.Position().Vect() - this_mcparticle_list[iparticle]->Position().Vect()).Mag() < 1e-4 &&
-              matched_genie_particle.PdgCode() == this_mcparticle_list[iparticle]->PdgCode()) {
-
-            lepton_traj = this_mcparticle_list[iparticle].get();
-            // this should only be true for one particle
-            assert(lepton_traj != NULL);
-          }
-        }
-      }
-
-      // TODO: why aren't there matches sometimes?
-      if (lepton_traj != NULL) {
-        interaction.lepton.length = util::MCParticleLength(*lepton_traj);
-        if (fProviderManager != NULL) {
-          interaction.lepton.contained_length = util::MCParticleContainedLength(*lepton_traj, fActiveVolumes);
+          // also get the end point from the trajectory
+          interaction.lepton.end = lepton_traj->EndPosition().Vect();
         }
         else {
-          interaction.lepton.contained_length = event::kUnfilled;
+          interaction.lepton.contained_length = 0;
+          interaction.lepton.length = 0;
+          // if we couldn't find a trajectory, set end to start
+          interaction.lepton.end = interaction.lepton.start;
+        }
+
+        q_labframe = nu.Nu().EndMomentum() - lepton.Momentum(0);
+        interaction.neutrino.q0_lab = q_labframe.E();
+        interaction.neutrino.modq_lab = q_labframe.P();
+      }
+
+      // Get CCQE energy from lepton info
+      interaction.neutrino.eccqe = \
+        util::ECCQE(interaction.lepton.momentum, interaction.lepton.energy);
+
+      // Hadronic system
+      for (int iparticle=0; iparticle<this_mcparticle_list.size(); iparticle++) {
+        event::FinalStateParticle fsp;
+        const simb::MCParticle& particle = *this_mcparticle_list[iparticle];
+
+        if (particle.Process() != "primary") {
+          continue;
+        }
+
+        fsp.pdg = particle.PdgCode();
+        fsp.energy = particle.Momentum(0).Energy();
+        fsp.momentum = particle.Momentum(0).Vect();
+        fsp.start = particle.Position(0).Vect();
+        fsp.status_code = particle.StatusCode();
+        fsp.is_primary = (particle.Process() == "primary");
+        fsp.length = util::MCParticleLength(particle);
+        if (fProviderManager != NULL) {
+          fsp.contained_length = util::MCParticleContainedLength(particle, fActiveVolumes);
+        }
+        else {
+          fsp.contained_length = event::kUnfilled;
         }
         // also get the end point from the trajectory
-        interaction.lepton.end = lepton_traj->EndPosition().Vect();
-      }
-      else {
-        interaction.lepton.contained_length = 0;
-        interaction.lepton.length = 0;
-        // if we couldn't find a trajectory, set end to start
-        interaction.lepton.end = interaction.lepton.start;
+        fsp.end = particle.EndPosition().Vect();
+
+        interaction.finalstate.push_back(fsp);
       }
 
-      q_labframe = nu.Nu().EndMomentum() - lepton.Momentum(0);
-      interaction.neutrino.q0_lab = q_labframe.E();
-      interaction.neutrino.modq_lab = q_labframe.P();
+      interaction.nfinalstate = interaction.finalstate.size();
+
+      // GENIE specific
+      if (genie_truth_is_valid) {
+        auto const& gtruth = gtruths_handle->at(i);
+        TLorentzVector q_nucframe(q_labframe);
+        // This nucleon momentum should be added to MCNeutrino so we don't
+        // have to rely on GTruth
+        const TLorentzVector& nucP4 = gtruth.fHitNucP4;
+        TVector3 nuc_boost(nucP4.BoostVector());
+        q_nucframe.Boost(nuc_boost);
+        interaction.neutrino.modq = q_nucframe.P();
+        interaction.neutrino.q0 = q_nucframe.E();
+      }
+
+      fEvent->truth.push_back(interaction);
     }
-
-    // Get CCQE energy from lepton info
-    interaction.neutrino.eccqe = \
-      util::ECCQE(interaction.lepton.momentum, interaction.lepton.energy);
-
-    // Hadronic system
-    for (int iparticle=0; iparticle<this_mcparticle_list.size(); iparticle++) {
-      event::FinalStateParticle fsp;
-      const simb::MCParticle& particle = *this_mcparticle_list[iparticle];
-
-      if (particle.Process() != "primary") {
-        continue;
-      }
-
-      fsp.pdg = particle.PdgCode();
-      fsp.energy = particle.Momentum(0).Energy();
-      fsp.momentum = particle.Momentum(0).Vect();
-      fsp.start = particle.Position(0).Vect();
-      fsp.status_code = particle.StatusCode();
-      fsp.is_primary = (particle.Process() == "primary");
-      fsp.length = util::MCParticleLength(particle);
-      if (fProviderManager != NULL) {
-        fsp.contained_length = util::MCParticleContainedLength(particle, fActiveVolumes);
-      }
-      else {
-        fsp.contained_length = event::kUnfilled;
-      }
-      // also get the end point from the trajectory
-      fsp.end = particle.EndPosition().Vect();
-
-      interaction.finalstate.push_back(fsp);
-    }
-
-    interaction.nfinalstate = interaction.finalstate.size();
-
-    // GENIE specific
-    if (genie_truth_is_valid) {
-      auto const& gtruth = gtruths_handle->at(i);
-      TLorentzVector q_nucframe(q_labframe);
-      // This nucleon momentum should be added to MCNeutrino so we don't
-      // have to rely on GTruth
-      const TLorentzVector& nucP4 = gtruth.fHitNucP4;
-      TVector3 nuc_boost(nucP4.BoostVector());
-      q_nucframe.Boost(nuc_boost);
-      interaction.neutrino.modq = q_nucframe.P();
-      interaction.neutrino.q0 = q_nucframe.E();
-    }
-
-    fEvent->truth.push_back(interaction);
   }
 
   fEvent->ntruth = fEvent->truth.size();
