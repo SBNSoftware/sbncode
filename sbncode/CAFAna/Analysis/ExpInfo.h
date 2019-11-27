a3377dcdbb42419f232d2320260d4b0065bdf17d
Bruno Zamorano <bzamoran@fnal.gov>
2019-11-27 09:58:16 -0600

    Adding nominal exposure for MicroBoone

--- sbncode/CAFAna/Analysis/ExpInfo.h
+++ sbncode/CAFAna/Analysis/ExpInfo.h
@@ -4,6 +4,7 @@ namespace ana
 
 {
   const double kPOTnominal = 6.6e20;
+  const double kPOTuBoone = 1.3e21;
   const double kBaselineSBND = 0.11;
   const double kBaselineMicroBoone = 0.47;
   const double kBaselineIcarus = 0.6; 
