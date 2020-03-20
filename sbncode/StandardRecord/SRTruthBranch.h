5fe8a5c15f154a0ea20e467b2a99cb29f3586c46
Fernanda Psihas <psihas@fnal.gov>
2020-03-20 16:17:07 -0500

    Adding new branch files I forgot in my previous commit

---
+++ sbncode/StandardRecord/SRTruthBranch.h
@@ -0,0 +1,30 @@
+////////////////////////////////////////////////////////////////////////
+// \file    SRTruthBranch.h
+////////////////////////////////////////////////////////////////////////
+#ifndef SRTRUTHBRANCH_H
+#define SRTRUTHBRANCH_H
+
+#include "SRTrueInteraction.h"
+
+#include <vector>
+
+namespace caf
+{
+  /// Vectors of reconstructed vertices found by various algorithms
+  class SRTruthBranch
+  {
+  public:
+    SRTruthBranch();
+    ~SRTruthBranch();
+            
+    std::vector<SRTrueInteraction> nu;   ///< Vector of true nu or cosmic
+    size_t                        nnu;   ///< Number of true nu or cosmic
+
+    void fillSizes();
+      
+  };
+  
+} // end namespace
+
+#endif // SRTRUTHBRANCH_H
+////////////////////////////////////////////////////////////////////////////
