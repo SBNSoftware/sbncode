54ebd4067656cbc0a01c249817489dca9b41e69d
Diana Patricia Mendez <dmendezme@bnl.gov>
2020-03-25 10:31:28 -0500

    Chage file path for demo script.

--- sbncode/SBNAna/anademo/demo_2020Workshop/make_demo.C
+++ sbncode/SBNAna/anademo/demo_2020Workshop/make_demo.C
@@ -14,9 +14,7 @@ void make_demo()
   //
   // Environment variables and wildcards work as arguments to
   // SpectrumLoader. As do SAM datasets.
-  const std::string fname = "larout.caf.root";
-    //"/sbnd/app/users/psihas/SBNCAF-dev/prodgenie_bnb_nu_cosmic_sbnd_GenieGen-20191122T115936_G4-20191122T200111_DetSim-20191122T205333_Reco-20191122T224902_0c973425-93ec-4a22-9e8f-d2fe0fd2d189_ReOp-20200324T001103.caf.root";  
-  ///sbnd/app/users/psihas/ExampleFiles/CAFs/prodgenie_bnb_nu_cosmic_sbnd_20191128_reco.caf.root";
+  const std::string fname = "/pnfs/icarus/persistent/users/dmendez/SBNAnaFiles/workshopdemo/numu.caf.root";
 
   // Source of events
   SpectrumLoader loader(fname);
