355ca499d52baef9572a463dd2a75bdc7d9e65e1
Diana Patricia Mendez <dmendezme@bnl.gov>
2020-03-24 19:11:28 -0500

    Workaround mc.nnu error.

--- sbncode/SBNAna/Cuts/TruthCuts.cxx
+++ sbncode/SBNAna/Cuts/TruthCuts.cxx
@@ -4,43 +4,43 @@
 
 namespace ana{
 
-	// const Cut kIsAntiNu([](const caf::SRProxy* sr){
-	// 	if(sr->mc.nnu == 0) return false;
-	// 	assert(sr->mc.nnu == 1);
-	// 	return sr->mc.nu[0].pdg < 0;
-	// });
+	const Cut kIsAntiNu([](const caf::SRProxy* sr){
+		if(int(sr->mc.nnu) == 0 ) return false;
+		assert( int(sr->mc.nnu) == 1);
+		return sr->mc.nu[0].pdg < 0;
+	});
 
-	// const Cut kIsNu([](const caf::SRProxy* sr){
-	// 	if(sr->mc.nnu == 0) return false;
-	// 	assert(sr->mc.nnu == 1);
-	// 	return sr->mc.nu[0].pdg > 0;
-	// });
+	const Cut kIsNu([](const caf::SRProxy* sr){
+		if(int(sr->mc.nnu) == 0) return false;
+		assert(int(sr->mc.nnu) == 1);
+		return sr->mc.nu[0].pdg > 0;
+	});
 
-	// const Cut kHasNu([](const caf::SRProxy* sr){
-	// 	if(sr->mc.nnu == 0) return false;
-	// 	assert(sr->mc.nnu == 1);
-	// 	return true;
-	// });
-
-
-	// const Cut kIsNue([](const caf::SRProxy* sr){
-	// 	return (sr->mc.nnu == 1 && abs(sr->mc.nu[0].pdg) ==12);
-	// });
-
-	// const Cut kIsNumu([](const caf::SRProxy* sr){
-	// 	return (sr->mc.nnu == 1 && abs(sr->mc.nu[0].pdg) ==14);
-	// });
-
-	// const Cut kIsNutau([](const caf::SRProxy* sr){
-	// 	return (sr->mc.nnu == 1 && abs(sr->mc.nu[0].pdg) ==16);
-	// });
+	const Cut kHasNu([](const caf::SRProxy* sr){
+		if(int(sr->mc.nnu) == 0) return false;
+		assert(int(sr->mc.nnu) == 1);
+		return true;
+	});
 
 
-	// const Cut kIsNC([](const caf::SRProxy* sr){
-	// 	if(sr->mc.nnu == 0) return false;
-	// 	assert(sr->mc.nnu == 1);
-	// 	return !sr->mc.nu[0].iscc;
-	// });
+	const Cut kIsNue([](const caf::SRProxy* sr){
+		return (int(sr->mc.nnu) == 1 && abs(sr->mc.nu[0].pdg) ==12);
+	});
+
+	const Cut kIsNumu([](const caf::SRProxy* sr){
+		return (int(sr->mc.nnu) == 1 && abs(sr->mc.nu[0].pdg) ==14);
+	});
+
+	const Cut kIsNutau([](const caf::SRProxy* sr){
+		return (int(sr->mc.nnu) == 1 && abs(sr->mc.nu[0].pdg) ==16);
+	});
+
+
+	const Cut kIsNC([](const caf::SRProxy* sr){
+		if(int(sr->mc.nnu) == 0) return false;
+		assert(int(sr->mc.nnu) == 1);
+		return !sr->mc.nu[0].iscc;
+	});
 
 
 }
