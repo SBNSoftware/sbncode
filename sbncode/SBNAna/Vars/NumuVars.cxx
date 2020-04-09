6c7999b52aa4546b850fd6f131b4b6a04f1b65ea
gputnam <gputnam@sbndgpvm04.fnal.gov>
2020-04-09 16:07:50 -0500

    Fix NumuVars for new calorimetry -- always use collection plane for now.

--- sbncode/SBNAna/Vars/NumuVars.cxx
+++ sbncode/SBNAna/Vars/NumuVars.cxx
@@ -18,7 +18,7 @@ namespace ana
 			    auto &trk = sr->reco.trk[trkIdx];
 			    
 			    //Find longest trk w/Chi2 for muon < Chi2 for pion
-			    bool isMuonLike = trk.chi2pid.chi2_pion > trk.chi2pid.chi2_muon;
+			    bool isMuonLike = trk.chi2pid[2].chi2_pion > trk.chi2pid[2].chi2_muon;
 			    if( isMuonLike && trk.len > best_len ){ //Chi2 muon < Chi2 pion
 			      // best_score = score;
 			      best_len = trk.len;
