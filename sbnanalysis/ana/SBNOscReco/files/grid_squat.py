01f189c8d92f957e822676e30ae10550e303cdfe
gputnam <gputnam@sbndgpvm04.fnal.gov>
2020-05-05 17:17:20 -0500

    Add anotehr filelist. Update script for busy-ing files to use xROOTd

--- sbnanalysis/ana/SBNOscReco/files/grid_squat.py
+++ sbnanalysis/ana/SBNOscReco/files/grid_squat.py
@@ -12,8 +12,11 @@ def run(filelists):
 # make the file seem busy
 def busyfile(fname,outfile="TMP"):
     print fname
+    process = subprocess.Popen(['pnfsToXRootD', fname], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    xroot_fname, _ = process.communicate()
+    print xroot_fname
     with open(outfile, "w") as f:
-        process = subprocess.call(['lar', '-c', 'eventdump.fcl', '-s', fname, '-n', '1'], stdout=f)
+        process = subprocess.call(['lar', '-c', 'eventdump.fcl', '-s', xroot_fname, '-n', '1'], stdout=f)
 
 def hello(lst):
     print "Running! On files:"
@@ -23,5 +26,6 @@ while True:
     hello(sys.argv[1:])
     run(sys.argv[1:])
     # run every 3 days
+    print "Sleeping!"
     time.sleep(3*24*60*60) 
     print "DONE!"
