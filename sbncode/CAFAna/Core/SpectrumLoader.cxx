5bd2b1d054b1b6496c5f7909b63190b9e644018f
Christopher Backhouse <bckhouse@fnal.gov>
2020-06-04 11:37:03 -0500

    Update to new systematics interface in SRProxy v00.06

--- sbncode/CAFAna/Core/SpectrumLoader.cxx
+++ sbncode/CAFAna/Core/SpectrumLoader.cxx
@@ -187,6 +187,8 @@ namespace ana
       // work from. Copying the whole StandardRecord is pretty expensive, so
       // modify it in place and revert it afterwards.
 
+      caf::SRProxySystController::BeginTransaction();
+
       bool shifted = false;
 
       double systWeight = 1;
@@ -257,7 +259,7 @@ namespace ana
        
       // Return StandardRecord to its unshifted form ready for the next
       // histogram.
-      caf::SRProxySystController::ResetSysts();
+      caf::SRProxySystController::Rollback();
     } // end for shiftdef
   }
 
