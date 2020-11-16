0d582a28f60ac6d72445a7c9df27e6d8b3a61af4
Edward Tyley <e.tyley@sheffield.ac.uk>
2020-11-16 10:54:54 -0600

    Revert "Revert "Tidied duplications in slice truth matching""
    This reverts commit a624cfb5e452953a6288ee0676d3dcaef188670b.

--- sbncode/StandardRecord/SRTruthMatch.cxx
+++ sbncode/StandardRecord/SRTruthMatch.cxx
@@ -10,12 +10,9 @@
 namespace caf
 {
   SRTruthMatch::SRTruthMatch():
-    visEinslc(std::numeric_limits<float>::signaling_NaN()),
-    visEcosmic(std::numeric_limits<float>::signaling_NaN()),
     eff(std::numeric_limits<float>::signaling_NaN()),
     pur(std::numeric_limits<float>::signaling_NaN()),
-    index(std::numeric_limits<int>::signaling_NaN()),
-    is_numucc_primary(std::numeric_limits<bool>::signaling_NaN())
+    index(std::numeric_limits<int>::signaling_NaN())
   {  }
 
   SRTruthMatch::~SRTruthMatch(){  }
@@ -23,8 +20,6 @@ namespace caf
   void SRTruthMatch::setDefault()
   {
     index             = -5;
-    visEinslc         = -5.0;
-    visEcosmic        = -5.0;
     eff               = -5.0;
     pur               = -5.0;
   }
