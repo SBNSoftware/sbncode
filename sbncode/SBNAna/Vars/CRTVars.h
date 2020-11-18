6e5c211f520861022c064fa470013e62e9452e48
Edward Tyley <e.tyley@sheffield.ac.uk>
2020-11-18 10:20:10 -0600

    Add CRT Vars

---
+++ sbncode/SBNAna/Vars/CRTVars.h
@@ -0,0 +1,15 @@
+#pragma once
+// Vars around the CRTs
+
+#include "CAFAna/Core/Var.h"
+#include "CAFAna/Core/MultiVar.h"
+
+namespace ana
+{
+  extern const MultiVar kCRTHit_Times;
+  extern const MultiVar kCRTHit_PEs;
+
+  extern const MultiVar kCRTHit_X;
+  extern const MultiVar kCRTHit_Y;
+  extern const MultiVar kCRTHit_Z;
+}
