df7d8e5a40aedf74ddc4a4c7c9a81c8c285d8a7b
Edward Tyley <e.tyley@sheffield.ac.uk>
2020-11-20 09:22:16 -0600

    Remember to update the header

--- sbncode/SBNAna/Cuts/NueCuts.h
+++ sbncode/SBNAna/Cuts/NueCuts.h
@@ -7,6 +7,7 @@ namespace ana
 {
 	extern const Cut kRecoShower;
 	extern const Cut kNueBasicCut;
+	extern const Cut kNueContainedND;
 	extern const Cut kNueContainedFD;
 	
 	const Cut kNueCut = kRecoShower && kNueBasicCut && (kRecoShower_ConversionGap < 2.1);	
