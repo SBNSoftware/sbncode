33cba15c6b3aa267a245dd8705e95ccb6595e6d3
Christopher Backhouse <bckhouse@fnal.gov>
2020-11-27 13:32:14 -0600

    Update to new srproxy version which also provides ability to make FlatRecord (and a new, easier to work with, FlatRecord format).

--- sbncode/FlatMaker/convert_to_flat.cc
+++ sbncode/FlatMaker/convert_to_flat.cc
@@ -45,21 +45,17 @@ int main(int argc, char** argv)
   tr->SetBranchAddress("rec", &event);
 
   // LZ4 is the fastest format to decompress. I get 3x faster loading with
-  // thiscompared to the default, and the files are only slightly larger.
+  // this compared to the default, and the files are only slightly larger.
   TFile fout(outname.c_str(), "RECREATE", "",
              ROOT::CompressionSettings(ROOT::kLZ4, 1));
 
-  // First, copy the POT information over
-  //  ((TTree*)fin->Get("sbnsubrun"))->CloneTree()->Write("sbnsubrun");
-
-  fout.mkdir("recTree")->cd();
-  TTree* trout = new TTree("rec", "rec");
+  TTree* trout = new TTree("recTree", "recTree");
   // On NOvA, had trouble with memory usage (because several trees are open at
   // once?). Setting the maximum buffer usage (per tree) to 3MB (10x less than
   // default) fixed it. But it doesn't seem necessary for now on SBN.
   //  trout->SetAutoFlush(-3*1000*1000);
 
-  flat::FlatRecord* rec = new flat::FlatRecord("rec.", trout, 0);//policy);
+  flat::Flat<caf::StandardRecord> rec(trout, "rec", "", 0);//policy);
 
   ana::Progress prog("Converting '"+inname+"' to '"+outname+"'");
   for(int i = 0; i < tr->GetEntries(); ++i){
@@ -71,18 +67,13 @@ int main(int argc, char** argv)
       if(bl < 150) event->mc.nu[0].baseline -= 10;
     }
 
-    rec->Fill(*event);
+    rec.Clear();
+    rec.Fill(*event);
     trout->Fill();
-
-    // This causes us to have much larger baskets, which seems like it should
-    // be more efficient for semi-random access of systs, but doesn't seem to
-    // help much in practice. It appears the limit is capped at 256e6.
-    if(i == 1000) rec->OptimizeBaskets(1000*1000*1000, 1.1, ""/*"d"*/);
   }
-  //  prog.Done();
+  prog.Done();
 
   trout->Write();
-  delete rec;
 
   TH1* hPOT = (TH1*)fin->Get("TotalPOT");
   TH1* hEvts = (TH1*)fin->Get("TotalEvents");
