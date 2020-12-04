27cc165cbccbba8219d739968904bad5cf56cad7
Christopher Backhouse <bckhouse@fnal.gov>
2020-12-04 07:53:42 -0600

    Port FileReducer, for making smaller CAFs based on cuts or reduction steps.

---
+++ sbncode/CAFAna/test/test_reducer.C
@@ -0,0 +1,19 @@
+#include "CAFAna/Core/FileReducer.h"
+
+#include "SBNAna/Cuts/Cuts.h"
+
+using namespace ana;
+
+void test_reducer(std::string fin,
+                  std::string fout = "reduced.root")
+{
+  FileReducer reducer(fin, fout);
+
+  // Only keep spills passing flash trigger
+  reducer.AddEventCut(kFlashTrigger);
+
+  // And when we do keep them, remove their true particle list
+  reducer.AddReductionStep(ClearTrueParticles);
+
+  reducer.Go();
+}
