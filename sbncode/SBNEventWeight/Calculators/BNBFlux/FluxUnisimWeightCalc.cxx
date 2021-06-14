4a7b80dd1bb9fdafe401f3cb2f7b98be2661c86b
Ooohu <393558728@qq.com>
2021-06-14 08:19:57 -0500

    MCParticle Handle just works after reset the environment?

--- sbncode/SBNEventWeight/Calculators/BNBFlux/FluxUnisimWeightCalc.cxx
+++ sbncode/SBNEventWeight/Calculators/BNBFlux/FluxUnisimWeightCalc.cxx
@@ -9,11 +9,14 @@
 //
 // Ported to/adapted for SBNCode, Dec 2020, A. Mastbaum
 //
-// Ported from ubcode; the structure is based on SBNEventweight/Calculator/CrossSections/GenieWeightCalc.cxx
-//		by K. Lin Jun. 2021
+// ----
+// Ported from ubcode; objects used here are based on
+//		SBNEventweight/Calculator/CrossSections/GenieWeightCalc.cxx
+//
+//  - GetWeight() function is re-introduced but it has bug to be fixed;
+//		by Keng Lin Jun. 2021
 
 
-//GetWeight() function is introduced but it has bug;
 
 #include "art/Framework/Principal/Event.h"
 #include "nugen/NuReweight/art/NuReweight.h"
@@ -47,8 +50,8 @@ public:
 
 	//Function for evaluating a specific weight
 	//enu - neutrino energy from simb::MCTruth; 
-	//ptype- charge particles label; 
-	//ntype - neutrino label, 
+	//ptype-  parent particles label: pi, k, k0, mu 
+	//ntype - neutrino flavor label: numu, numubar, nue, nuebar 
 	//noNeg - formulas for weights depending on input histograms.
   double MicroBooNEWeightCalc(double enu, int ptype, int ntype, int uni, bool noNeg);
 
@@ -71,6 +74,8 @@ private:
   double fRWneg[4][4][200];
   bool PosOnly{false};
 
+  std::string fWeightCalc{}; // if you want MiniBooNE or MicroBooNE calculator; CHECK, maybe no need this?
+
   DECLARE_WEIGHTCALC(FluxUnisimWeightCalc)
 };
 
@@ -83,101 +88,105 @@ private:
 //	- Set fMode;
 //- Configure calculator setting: 
 //	- Load histograms and save them;
+//
+//To do list
+//[ ] locate root files (search_path does not work).
 
-void FluxUnisimWeightCalc::Configure(fhicl::ParameterSet const& p,
-                                CLHEP::HepRandomEngine& engine) {
-  // Global config
-  // Calculator config
+  // FHiCL configuration reads as
+  //module_type: "SBNEventWeight"//taken care in WeightManager.h
+  //genie_module_label: generator//taken care in WeightManager.h
 	//horncurrent: {
 	//	<<  Reweighting  Environment >>
-	//type: FluxUnisim
-	//		  parameter_list: ["horncurrent"]
-	//		  random_seed: 7
-	//		  mode: multisim
-	//		  number_of_multisims: 1000
+	//		  type: FluxUnisim //taken care in EventWeightParameterSet.cxx
+	//		  random_seed: 7   //taken care in WeightManager.h
+	//		  parameter_list: ["horncurrent"] //processed in EventWeightParameterSet.cxx
+	//		  mode: multisim                  //processed in EventWeightParameterSet.cxx
+	//		  number_of_multisims: 1000       //processed in EventWeightParameterSet.cxx
 	//
-	//	<<  Calculator Setting >>
+	//	<<  Calculator Settings >>
 	//		  CentralValue_hist_file: "*.root"
 	//		  PositiveSystematicVariation_hist_file: "*.root"
 	//		  NegativeSystematicVariation_hist_file: ".root"
 	//		  scale_factor_pos: 1
 	//		  scale_factor_neg: 1
-	//		  weight_calculator: "MicroBooNE" # "MicroBooNE" OR "MiniBooNE"
+	//		  weight_calculator: "MicroBooNE" //All MicroBooNE shoul
+	//
 	//	<< Outdated >>
 	//		  use_MiniBooNE_random_numbers: false
 	//			 }
+	//
+	//Notation: taken care - no need to set up here; can be used directly
   
-
+void FluxUnisimWeightCalc::Configure(fhicl::ParameterSet const& p,
+                                CLHEP::HepRandomEngine& engine) {
+  //Collect FHiCL parameters:
+  //0. Global config
   fGenieModuleLabel = p.get<std::string>("genie_module_label");//use this label to get MC*Handle
   const fhicl::ParameterSet& pset = p.get<fhicl::ParameterSet>(GetName());
 
-	//Collect FHiCL parameters:
+ //1. << Reweighting Environment >>
   auto const& pars = pset.get<std::vector<std::string> >("parameter_list");
-//  auto const& parsigmas = pset.get<std::vector<float> >("parameter_sigma");
-
-  //ErrorMsg
-//  if (pars.size() != parsigmas.size()) {
-//    throw cet::exception(__PRETTY_FUNCTION__) << GetName() << ": "
-//      << "parameter_list and parameter_sigma length mismatch."
-//      << std::endl;
-//  }
-
-  int number_of_multisims = pset.get<int>("number_of_multisims", 1);
-  std::string mode = pset.get<std::string>("mode");//3 types: multisim/pmNsigma/fixed
-
-  // Set up parameters
-  fParameterSet.Configure(GetFullName(), mode, number_of_multisims);
-
   for (size_t i=0; i<pars.size(); i++) {
 	//Check, no sigma is used in this script.
     fParameterSet.AddParameter(pars[i], 1);//parsigmas[i]);
   }
 
-  fParameterSet.Sample(engine);
+  std::string mode = pset.get<std::string>("mode");//3 types: multisim/pmNsigma/fixed
 
-  // Set up GENIE with the currently active tune
-//  evgb::SetEventGeneratorListAndTune();
-//  fTuneName = evgb::ExpandEnvVar("${GENIE_XSEC_TUNE}");
+  int number_of_multisims = pset.get<int>("number_of_multisims", 1);
+  fParameterSet.Configure(GetFullName(), mode, number_of_multisims);//set fParameterSet members
+  fParameterSet.Sample(engine);//random_seed is loade
 
-  // Set up reweighters
-  fWeightArray.resize(fParameterSet.fNuniverses);
+  fWeightArray.resize(number_of_multisims);
+//  fWeightArray.resize(fParameterSet.fNuniverses); //same thing, Check
 
-//  for (auto const& it : fParameterSet.fParameterMap) {
-//    std::string name = it.first.fName;
-//    std::cout << GetFullName() << ": Setting up " << name << std::endl;
+ //2. << Calculator Settings >>
+//  CHECK, search_path function is not available;
+//  cet::search_path sp("FW_SEARCH_PATH");
+
+ /// Grab the histogram related to the CV
+  std::string dataInput1	= pset.get< std::string >("CentralValue_hist_file");
+//  std::string cvfile		= sp.find_file(dataInput1);
+//  TFile fcv(Form("%s",cvfile.c_str()));
+
+//  /// Grab the histogram related to the variation 
+//  std::string dataInput2pos	= pset.get< std::string >("PositiveSystematicVariation_hist_file");
+//  std::string rwfilepos		= sp.find_file(dataInput2pos);
+//  TFile frwpos(Form("%s", rwfilepos.c_str()));
 //
-//    for (size_t i=0; i<fParameterSet.fNuniverses; i++) {
-//      rwgt::NuReweight& rw = fWeightArray[i];
+//  std::string dataInput2neg	= pset.get< std::string >("NegativeSystematicVariation_hist_file");
+//  std::string rwfileneg		= sp.find_file(dataInput2neg);
+//  TFile frwneg(Form("%s", rwfileneg.c_str()));
 //
-//      // Axial mass for NC elastic
-//      if (name == "NCELaxial")
-//        rw.ReweightNCEL(it.second[i], 0);
-//      // Strange axial form factor for NC elastic
-//      else if (name == "NCELeta")
-//        rw.ReweightNCEL(0, it.second[i]);
-//      // Axial mass for CC quasi-elastic
-//      else {
-//        throw cet::exception(__PRETTY_FUNCTION__) << GetName() << ": "
-//          << "Unknown GENIE parameter " << name << std::endl;
-//      }
-//    }
-//  }//next weights?
+//  for (int iptyp=0;iptyp<4;iptyp++) {
+//	  for (int intyp=0;intyp<4;intyp++) {
+//		  for (int ibin=0;ibin<200;ibin++) { //Grab events from ibin+1 
+//
+//			  fCV[iptyp][intyp][ibin]=(dynamic_cast<TH1F*> (fcv.Get(Form("h5%d%d",ptype[iptyp],ntype[intyp]))))->GetBinContent(ibin+1);
+//			  fRWpos[iptyp][intyp][ibin]=(dynamic_cast<TH1F*> (frwpos.Get(Form("h5%d%d",ptype[iptyp],ntype[intyp]))))->GetBinContent(ibin+1);
+//			  fRWneg[iptyp][intyp][ibin]=(dynamic_cast<TH1F*> (frwneg.Get(Form("h5%d%d",ptype[iptyp],ntype[intyp]))))->GetBinContent(ibin+1);
+//
+//		  }// energy bin
+//	  }//   type of neutrinos
+//  }//     type of hadron parent 
+//
+//
+//  fcv.Close();
+//  frwpos.Close();
+//  frwneg.Close(); 
 
-  // Configure reweight drivers
-//  for (auto& rw : fWeightArray) {
-//    rw.Configure();
-//  }
+  fScalePos 		= pset.get<double>("scale_factor_pos");
+  fScaleNeg 		= pset.get<double>("scale_factor_neg");
+
+  fWeightCalc		= pset.get<std::string>("weight_calculator");//CHECK, no need this?
+
 }
 
-//K: To be updated version requires:
+//K: New version requires:
 // GetWeight() needs:
 // - art::Handle< std::vector<simb::MCFlux> > 
 // - art::Handle< std::vector<simb::MCTruth> >
 // - boolean (true - identical in histograms?)
-//
-// == Note that inu would be used in mclist.at(inu) in the following;
-// [ ] mclist not working;
-// [ ] fluxlist not working;
 
 std::vector<float> FluxUnisimWeightCalc::GetWeight(art::Event& e, size_t inu) {
 	std::vector<float> weights(fWeightArray.size(), 1);
@@ -186,14 +195,10 @@ std::vector<float> FluxUnisimWeightCalc::GetWeight(art::Event& e, size_t inu) {
 //
 			//Collect the event's Flux information
 			//     This specifically deals with the neutrino type and parentage
-//CHECK - unable to get parent particle pdg;
-//CMakeFiles/sbncode_SBNEventWeight_Calculators_BNBFlux.dir/FluxUnisimWeightCalc.cxx.o: In function `simb::MCParticle::Momentum(int) const':
-///cvmfs/larsoft.opensciencegrid.org/products/nusimdata/v1_22_02/include/nusimdata/SimulationBase/MCParticle.h:220: undefined reference to `simb::MCTrajectory::Momentum(unsigned long) const'
 //
-//
-//			art::Handle< std::vector<simb::MCFlux> > mcFluxHandle;
-//			e.getByLabel(fGenieModuleLabel,mcFluxHandle);
-//			std::vector<simb::MCFlux> const& fluxlist = *mcFluxHandle;
+			art::Handle< std::vector<simb::MCFlux> > mcFluxHandle;
+			e.getByLabel(fGenieModuleLabel,mcFluxHandle);
+			std::vector<simb::MCFlux> const& fluxlist = *mcFluxHandle;
 
 
 			//Collect event's MC truth information
@@ -220,27 +225,25 @@ std::vector<float> FluxUnisimWeightCalc::GetWeight(art::Event& e, size_t inu) {
 				int ptype = std::numeric_limits<int>::max(); 
 				int ntype = std::numeric_limits<int>::max();
 
-//DUMMY test, make it works CHECK
-//				if (      fluxlist[0].fptype==13) ptype = 0;
 				// Discover the neutrino parent type
 				//     This contains the neutrino's parentage information
-//				if (      fluxlist[inu].fptype==13  || fluxlist[inu].fptype==-13  ) ptype = 0;
-//				else if ( fluxlist[inu].fptype==211 || fluxlist[inu].fptype==-211 ) ptype = 1;
-//				else if ( fluxlist[inu].fptype==130                               ) ptype = 2;
-//				else if ( fluxlist[inu].fptype==321 || fluxlist[inu].fptype==-321 ) ptype = 3;                                    
-//				else {
-//					throw cet::exception(__FUNCTION__) << GetName()<<"::Unknown ptype "<<fluxlist[0].fptype<< std::endl;
-//				}
-//
-//				// Discover the neutrino type
-//				//     This contains the neutrino's flavor information
-//				if (      fluxlist[inu].fntype== 12  ) ntype=0;
-//				else if ( fluxlist[inu].fntype==-12  ) ntype=1;
-//				else if ( fluxlist[inu].fntype== 14  ) ntype=2;
-//				else if ( fluxlist[inu].fntype==-14  ) ntype=3;
-//				else {
-//					throw cet::exception(__FUNCTION__) << GetName()<<"::Unknown ntype "<<fluxlist[0].fntype<< std::endl;
-//				}
+				if (      fluxlist[inu].fptype==13  || fluxlist[inu].fptype==-13  ) ptype = 0;
+				else if ( fluxlist[inu].fptype==211 || fluxlist[inu].fptype==-211 ) ptype = 1;
+				else if ( fluxlist[inu].fptype==130                               ) ptype = 2;
+				else if ( fluxlist[inu].fptype==321 || fluxlist[inu].fptype==-321 ) ptype = 3;                                    
+				else {
+					throw cet::exception(__FUNCTION__) << GetName()<<"::Unknown ptype "<<fluxlist[0].fptype<< std::endl;
+				}
+
+				// Discover the neutrino type
+				//     This contains the neutrino's flavor information
+				if (      fluxlist[inu].fntype== 12  ) ntype=0;
+				else if ( fluxlist[inu].fntype==-12  ) ntype=1;
+				else if ( fluxlist[inu].fntype== 14  ) ntype=2;
+				else if ( fluxlist[inu].fntype==-14  ) ntype=3;
+				else {
+					throw cet::exception(__FUNCTION__) << GetName()<<"::Unknown ntype "<<fluxlist[0].fntype<< std::endl;
+				}
 
 				// Collect neutrino energy
 				//CHECK cant get mclist info; same error as fluxlist
