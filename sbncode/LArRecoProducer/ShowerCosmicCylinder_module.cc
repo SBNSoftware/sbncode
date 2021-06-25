5daaabba4b79d4618256c9de492e6efd0b3912b5
Edward Tyley <e.tyley@sheffield.ac.uk>
2021-06-25 07:56:38 -0500

    Tidy up cosmic cylinder producer

--- sbncode/LArRecoProducer/ShowerCosmicCylinder_module.cc
+++ sbncode/LArRecoProducer/ShowerCosmicCylinder_module.cc
@@ -22,11 +22,9 @@
 #include "messagefacility/MessageLogger/MessageLogger.h"
 
 #include "canvas/Persistency/Common/FindManyP.h"
-#include "lardataobj/RecoBase/Hit.h"
 #include "lardataobj/RecoBase/PFParticle.h"
 #include "lardataobj/RecoBase/PFParticleMetadata.h"
 #include "lardataobj/RecoBase/Shower.h"
-#include "lardataobj/RecoBase/Slice.h"
 #include "lardataobj/RecoBase/SpacePoint.h"
 #include "lardataobj/RecoBase/Track.h"
 
@@ -60,6 +58,13 @@ class sbn::ShowerCosmicCylinder : public art::EDProducer {
   const art::InputTag fShowerLabel;
 
   const float fMinShowerEnergy;
+
+  const std::vector<art::Ptr<recob::PFParticle>> GetCosmicPFPs(const std::vector<art::Ptr<recob::PFParticle>>& pfps,
+      const art::FindManyP<larpandoraobj::PFParticleMetadata> fmPFPMeta) const;
+
+  const float FindShowerResidual(const recob::Shower& shower,
+      const std::vector<art::Ptr<recob::PFParticle>>& cosmicPFPs,
+      const art::FindManyP<recob::SpacePoint>& fmPFPSP) const;
 };
 
 sbn::ShowerCosmicCylinder::ShowerCosmicCylinder(fhicl::ParameterSet const& p)
@@ -76,24 +81,15 @@ void sbn::ShowerCosmicCylinder::produce(art::Event& e)
 {
   //Get the showers
   auto const showerHandle = e.getValidHandle<std::vector<recob::Shower>>(fShowerLabel);
-  auto const sliceHandle = e.getValidHandle<std::vector<recob::Slice>>(fPandoraLabel);
   auto const pfpHandle = e.getValidHandle<std::vector<recob::PFParticle>>(fPandoraLabel);
   // auto const spHandle = e.getValidHandle<std::vector<recob::SpacePoint> >(fPandoraLabel);
 
   std::vector<art::Ptr<recob::Shower>> showers;
   art::fill_ptr_vector(showers, showerHandle);
 
-  std::vector<art::Ptr<recob::Slice>> slices;
-  art::fill_ptr_vector(slices, sliceHandle);
-
   std::vector<art::Ptr<recob::PFParticle>> pfps;
   art::fill_ptr_vector(pfps, pfpHandle);
 
-  const art::FindManyP<recob::PFParticle> fmSlicePFP(sliceHandle, e, fPandoraLabel);
-  if (!fmSlicePFP.isValid()) {
-    throw cet::exception("ShowerCosmicCylinder") << "Slice-PFP association is somehow not valid. Stopping";
-    return;
-  }
   const art::FindManyP<larpandoraobj::PFParticleMetadata> fmPFPMeta(pfpHandle, e, fPandoraLabel);
   if (!fmPFPMeta.isValid()) {
     throw cet::exception("ShowerCosmicCylinder") << "PFP-Meta association is somehow not valid. Stopping";
@@ -105,35 +101,7 @@ void sbn::ShowerCosmicCylinder::produce(art::Event& e)
     return;
   }
 
-  // Find the primary cosmic PFPs
-  std::vector<art::Ptr<recob::PFParticle>> cosmicPFPs;
-  for (auto const& slice : slices) {
-    std::vector<art::Ptr<recob::PFParticle>> slicePFPs(fmSlicePFP.at(slice.key()));
-
-    if (slicePFPs.empty())
-      continue;
-
-    // Find the Primary PFP
-    std::vector<art::Ptr<recob::PFParticle>> pfpPrimaries;
-    for (auto const& pfp : slicePFPs) {
-      if (pfp->IsPrimary())
-        pfpPrimaries.push_back(pfp);
-    }
-
-    // We should always have 1 primary
-    if (pfpPrimaries.size() != 1)
-      throw cet::exception("ShowerCosmicCylinder") << "Wrong number of primaries in slice: " << pfpPrimaries.size() << ". Stopping";
-
-    // Ignore anything that is not a clear cosmic
-    // TODO: Should expand to check for T0 tags from CRT/Flash matching
-    auto const& pfpMetaVec(fmPFPMeta.at(pfpPrimaries.front().key()));
-    if (pfpMetaVec.size() != 1)
-      throw cet::exception("ShowerCosmicCylinder") << "Wrong metadata entries for PFP: " << pfpMetaVec.size() << ". Stopping";
-    if (!pfpMetaVec.front()->GetPropertiesMap().count("IsClearCosmic"))
-      continue;
-
-    cosmicPFPs.push_back(pfpPrimaries.front());
-  } // end pfp: slicePFPs
+  const std::vector<art::Ptr<recob::PFParticle>> cosmicPFPs(GetCosmicPFPs(pfps, fmPFPMeta));
 
   std::unique_ptr<std::vector<float>> residualCol(std::make_unique<std::vector<float>>());
   std::unique_ptr<art::Assns<recob::Shower, float>> residualAssns(std::make_unique<art::Assns<recob::Shower, float>>());
@@ -144,19 +112,7 @@ void sbn::ShowerCosmicCylinder::produce(art::Event& e)
     if (shower->best_plane() < 0 || shower->Energy().at(shower->best_plane()) < fMinShowerEnergy)
       continue;
 
-    float res(std::numeric_limits<float>::max());
-    const TVector3 showerStart3(shower->ShowerStart());
-    const geo::Point_t showerStart(showerStart3.X(), showerStart3.Y(), showerStart3.Z());
-
-    // Loop over the cosmic PFPs and find the closest
-    for (auto const& cosmicPFP : cosmicPFPs) {
-      auto const& pfpSPs(fmPFPSP.at(cosmicPFP.key()));
-      // Loop over all of the spacepoints in the PFP and find the closest
-      for (auto const& sp : pfpSPs) {
-        const float dist((showerStart - sp->position()).r());
-        res = std::min(res, dist);
-      }
-    }
+    const float res(FindShowerResidual(*shower, cosmicPFPs, fmPFPSP));
 
     residualCol->push_back(res);
     util::CreateAssn(*this, e, *residualCol, shower, *residualAssns);
@@ -164,6 +120,51 @@ void sbn::ShowerCosmicCylinder::produce(art::Event& e)
 
   e.put(std::move(residualCol));
   e.put(std::move(residualAssns));
+}
+
+const std::vector<art::Ptr<recob::PFParticle>> sbn::ShowerCosmicCylinder::GetCosmicPFPs(
+    const std::vector<art::Ptr<recob::PFParticle>>& pfps,
+    const art::FindManyP<larpandoraobj::PFParticleMetadata> fmPFPMeta) const
+{
+  std::vector<art::Ptr<recob::PFParticle>> cosmicPFPs;
+  for (auto const& pfp : pfps) {
+
+    if (!pfp->IsPrimary())
+      continue;
+
+    auto const& pfpMetaVec(fmPFPMeta.at(pfp.key()));
+    if (pfpMetaVec.size() != 1)
+      throw cet::exception("ShowerCosmicCylinder") << "Wrong metadata entries for PFP: " << pfpMetaVec.size() << ". Stopping";
+
+    // Ignore anything that is not a clear cosmic
+    if (!pfpMetaVec.front()->GetPropertiesMap().count("IsClearCosmic"))
+      continue;
+
+    // TODO: Should expand to check for T0 tags from CRT/Flash matching
+
+    cosmicPFPs.push_back(pfp);
+  }
+  return cosmicPFPs;
+}
+
+const float sbn::ShowerCosmicCylinder::FindShowerResidual(const recob::Shower& shower,
+    const std::vector<art::Ptr<recob::PFParticle>>& cosmicPFPs,
+    const art::FindManyP<recob::SpacePoint>& fmPFPSP) const
+{
+  float res(std::numeric_limits<float>::max());
+  const TVector3 showerStart3(shower.ShowerStart());
+  const geo::Point_t showerStart(showerStart3.X(), showerStart3.Y(), showerStart3.Z());
+
+  // Loop over the cosmic PFPs and find the closest
+  for (auto const& cosmicPFP : cosmicPFPs) {
+    auto const& pfpSPs(fmPFPSP.at(cosmicPFP.key()));
+    // Loop over all of the spacepoints in the PFP and find the closest
+    for (auto const& sp : pfpSPs) {
+      const float dist((showerStart - sp->position()).r());
+      res = std::min(res, dist);
+    }
+  }
+  return res;
 }
 
 DEFINE_ART_MODULE(sbn::ShowerCosmicCylinder)
