e7e8d418b8b9995be793378fe74b449ab3673c7c
gputnam <gputnam@icarusgpvm03.fnal.gov>
2021-10-14 13:01:29 -0500

    Update track area hit finder to work in ICARUS. Also make work optionally with Raw-Digits instead of Wires.

--- sbncode/TPCReco/TrackHit/TrackAreaHit_module.cc
+++ sbncode/TPCReco/TrackHit/TrackAreaHit_module.cc
@@ -21,17 +21,20 @@
 #include "larcoreobj/SimpleTypesAndConstants/RawTypes.h"
 #include "larcorealg/Geometry/Exceptions.h"
 
+#include "lardataobj/RawData/RawDigit.h"
 #include "lardataobj/RecoBase/Track.h"
 #include "lardataobj/RecoBase/TrackTrajectory.h"
 #include "lardataobj/RecoBase/Trajectory.h"
 #include "lardataobj/RecoBase/Wire.h"
 #include "lardataobj/RecoBase/Hit.h"
 #include "lardataobj/RecoBase/TrackHitMeta.h"
+#include "lardataobj/AnalysisBase/T0.h"
 #include "larcore/Geometry/Geometry.h"
 #include "larcorealg/Geometry/GeometryCore.h"
 #include "lardataalg/DetectorInfo/DetectorPropertiesStandard.h"
 #include "lardata/DetectorInfoServices/DetectorPropertiesService.h"
 #include "lardata/DetectorInfoServices/DetectorClocksService.h"
+#include "larsim/Simulation/LArG4Parameters.h"
 
 #include <memory>
 
@@ -56,14 +59,20 @@ public:
   void produce(art::Event& e) override;
 
   recob::Hit MakeHit(const recob::Wire &wire, const geo::WireID &wireID, const geo::GeometryCore *geo, raw::TDCtick_t startTick, raw::TDCtick_t endTick) const;
+  recob::Hit MakeHit(const raw::RawDigit &digit, const geo::WireID &wireID, const geo::GeometryCore *geo, raw::TDCtick_t startTick, raw::TDCtick_t endTick) const;
 
 private:
 
   art::InputTag fTrackLabel;
   art::InputTag fWireLabel;
+  std::vector<art::InputTag> fDigitLabels;
+  std::string fT0Label;
   float fSignalShapingWindow;
+  float fTransverseDiffusionScale; 
+  float fLongitudinalDiffusionScale;
   bool fAppendExtraHit;
   bool fPrependExtraHit;
+  bool fUseWires;
 
 };
 
@@ -71,13 +80,18 @@ sbn::TrackAreaHit::TrackAreaHit(fhicl::ParameterSet const& p)
   : EDProducer{p},
     fTrackLabel(p.get<art::InputTag>("TrackLabel", "pandoraTrack")),
     fWireLabel(p.get<art::InputTag>("WireLabel", "calowire")),
+    fDigitLabels(p.get<std::vector<art::InputTag>>("DigitLabels", {})),
+    fT0Label(p.get<std::string>("T0Label", "")),
     fSignalShapingWindow(p.get<float>("SignalShapingWindow")),
+    fTransverseDiffusionScale(p.get<float>("TransverseDiffusionScale")),
+    fLongitudinalDiffusionScale(p.get<float>("LongitudinalDiffusionScale")),
     fAppendExtraHit(p.get<bool>("AppendExtraHit", false)),
-    fPrependExtraHit(p.get<bool>("PrependExtraHit", false))
+    fPrependExtraHit(p.get<bool>("PrependExtraHit", false)),
+    fUseWires(p.get<bool>("UseWires", true))
 {
   produces<std::vector<recob::Hit>>();
   produces<art::Assns<recob::Track, recob::Hit, recob::TrackHitMeta>>();
-  produces<art::Assns<recob::Hit, recob::Wire>>();
+  // produces<art::Assns<recob::Hit, recob::Wire>>();
 }
 
 recob::Hit sbn::TrackAreaHit::MakeHit(const recob::Wire &wire, const geo::WireID &wireID, const geo::GeometryCore *geo, raw::TDCtick_t startTick, raw::TDCtick_t endTick) const {
@@ -170,12 +184,68 @@ recob::Hit sbn::TrackAreaHit::MakeHit(const recob::Wire &wire, const geo::WireID
   return thisHit;
 }
 
+recob::Hit sbn::TrackAreaHit::MakeHit(const raw::RawDigit &digit, const geo::WireID &wireID, const geo::GeometryCore *geo, raw::TDCtick_t startTick, raw::TDCtick_t endTick) const {
+
+  // clamp the tick range inside the digits
+  startTick = std::max(0, startTick);
+  endTick = std::min((int)digit.NADC()-1, endTick);
+
+  // garbage values for some fields which are not applicable
+  raw::ChannelID_t hitChannel = geo->PlaneWireToChannel(wireID);
+  raw::TDCtick_t hitStartTick = startTick;
+  raw::TDCtick_t hitEndTick = endTick;
+  float hitPeakTime = (startTick + endTick) / 2.; // recob::Hit time unit is also [ticks] 
+  float hitSigmaPeakTime = (endTick - startTick) / 2.; 
+  float hitRMS = (endTick - startTick) / 2.;
+  float hitPeakAmplitude = 0.;
+  float hitSigmaPeakAmplitude = 0.;
+  float hitSumADC = 0.; // we're going to set this one
+  float hitIntegral = 0.;  // this one too
+  float hitSigmaIntegral = 0.; // TODO: this one?
+  short int hitMultiplicity = 0; // no pulse trains here
+  short int hitLocalIndex = 0;
+  float hitGoodnessOfFit = -999999;
+  short int hitNDF = 0;
+  geo::View_t hitView = geo->View(wireID);
+  geo::SigType_t hitSignalType = geo->SignalType(wireID);
+  geo::WireID hitWire = wireID;
+  
+  for (int i_adc = startTick; i_adc <= endTick; i_adc++) {
+    hitSumADC += digit.ADC(i_adc);
+    hitIntegral += digit.ADC(i_adc);
+  }
+
+  // construct the hit
+  recob::Hit thisHit(
+    hitChannel,
+    hitStartTick,
+    hitEndTick,
+    hitPeakTime,
+    hitSigmaPeakTime,
+    hitRMS,
+    hitPeakAmplitude,
+    hitSigmaPeakAmplitude,
+    hitSumADC,
+    hitIntegral,
+    hitSigmaIntegral,
+    hitMultiplicity,
+    hitLocalIndex,
+    hitGoodnessOfFit,
+    hitNDF,
+    hitView,
+    hitSignalType,
+    hitWire
+  );
+
+  return thisHit;
+}
+
 void sbn::TrackAreaHit::produce(art::Event& e)
 {
   // output data products
   std::unique_ptr<art::Assns<recob::Track, recob::Hit, recob::TrackHitMeta>> assn(new art::Assns<recob::Track, recob::Hit, recob::TrackHitMeta>);
   std::unique_ptr<std::vector<recob::Hit>> outHits(new std::vector<recob::Hit>);
-  std::unique_ptr<art::Assns<recob::Hit, recob::Wire>> wireAssn(new art::Assns<recob::Hit, recob::Wire>);
+  // std::unique_ptr<art::Assns<recob::Hit, recob::Wire>> wireAssn(new art::Assns<recob::Hit, recob::Wire>);
 
   art::PtrMaker<recob::Hit> hitPtrMaker{e};
 
@@ -186,19 +256,47 @@ void sbn::TrackAreaHit::produce(art::Event& e)
   std::vector<art::Ptr<recob::Track>> tracks;
   art::fill_ptr_vector(tracks, track_handle);
 
-  art::Handle<std::vector<recob::Wire>> wire_handle;
-  e.getByLabel(fWireLabel, wire_handle);
-
   std::vector<art::Ptr<recob::Wire>> wires;
-  art::fill_ptr_vector(wires, wire_handle);
+  std::vector<art::Ptr<raw::RawDigit>> digits;
+
+  if (fUseWires) {
+    art::Handle<std::vector<recob::Wire>> wire_handle;
+    e.getByLabel(fWireLabel, wire_handle);
+
+    art::fill_ptr_vector(wires, wire_handle);
+  }
+  else {
+    for (const art::InputTag &l: fDigitLabels) {
+      art::Handle<std::vector<raw::RawDigit>> digit_handle;
+      e.getByLabel(l, digit_handle);
+      art::fill_ptr_vector(digits, digit_handle);
+    }
+  }
+
+  art::FindManyP<anab::T0> fmT0(tracks, e, fT0Label);
 
   const geo::GeometryCore *geo = lar::providerFrom<geo::Geometry>();
   auto const dclock = art::ServiceHandle<detinfo::DetectorClocksService const>()->DataFor(e);
   auto const dprop =
     art::ServiceHandle<detinfo::DetectorPropertiesService const>()->DataFor(e, dclock);
 
+  // auto const g4param = art::ServiceHandle<sim::LArG4Parameters const>();
+  art::ServiceHandle<sim::LArG4Parameters const> g4param;
+
+  std::set<unsigned> channels;
+
   for (unsigned i = 0; i < tracks.size(); i++) {
     const recob::Track &track = *tracks[i];
+
+    // Look up if the track was shifted by some T0
+    double track_t0 = 0.;
+    if (fT0Label.size()) {
+      const std::vector<art::Ptr<anab::T0>> &t0 = fmT0.at(i);
+      if (t0.size()) {
+        track_t0 = t0.at(0)->Time() / 1e3 /* ns -> us */;
+      }
+      std::cout << "T0: " << t0.at(0)->Time() << " corr: " << track_t0 << std::endl;
+    }
 
     // get the cryostat
     geo::CryostatID cid;
@@ -229,8 +327,8 @@ void sbn::TrackAreaHit::produce(art::Event& e)
         if (!geo->TPC(planeID).ContainsPosition(thispoint)) continue;
 
         // calculate overlap with wires
-        double thiswirecoord = geo->WireCoordinate(thispoint.Y(), thispoint.Z(), planeID);         
-        double nextwirecoord = geo->WireCoordinate(nextpoint.Y(), nextpoint.Z(), planeID);         
+        double thiswirecoord = geo->WireCoordinate(thispoint_p, planeID);         
+        double nextwirecoord = geo->WireCoordinate(nextpoint_p, planeID);         
 
         int wireStart = std::nearbyint((thiswirecoord >= nextwirecoord) ? std::floor(thiswirecoord) : std::ceil(thiswirecoord));
         int wireEnd   = std::nearbyint((thiswirecoord >= nextwirecoord) ? std::floor(nextwirecoord) : std::ceil(nextwirecoord));
@@ -263,15 +361,15 @@ void sbn::TrackAreaHit::produce(art::Event& e)
         // again continue if this wouldn't cross a new wire
         if (wireStart == wireEnd) continue;
 
-
 	// get the integration window 
 	float x = thispoint.X();
-	// TODO SCE(??)
 	recob::Track::Vector_t dir_v = track.DirectionAtPoint(j);
 	TVector3 dir(dir_v.X(), dir_v.Y(), dir_v.Z());
 	
 	// Get time from X [ticks]
-	float time = dprop.ConvertXToTicks(x, planeID);
+	//
+	// Undo any T0 correction to the track to get back to the hits
+	float time = dprop.ConvertXToTicks(x, planeID) + track_t0 * geo->TPC(planeID).DriftDir()[0] / dclock.TPCClock().TickPeriod();
 
 	// get the geometric time window [ticks]
 	//
@@ -282,6 +380,11 @@ void sbn::TrackAreaHit::produce(art::Event& e)
 	double cosgamma = std::abs(std::sin(angleToVert)*dir.Y() + std::cos(angleToVert)*dir.Z()); 
 	double effpitch = geo->WirePitch() / cosgamma;
 
+        // Add in the contribution from diffusion
+        float xdrift = abs(x - geo->TPC(planeID).PlaneLocation(0)[0]); // Copied from G4 / DriftIonizationElectrons
+        float tdrift = xdrift / dprop.DriftVelocity();
+        effpitch += fTransverseDiffusionScale * sqrt(2*tdrift*g4param->TransverseDiffusion());
+
 	double proj_length = abs(effpitch * dir.X() / sqrt(1 - dir.X() * dir.X()));
         // if the projected length is too big, clamp it to the track length
         proj_length = std::min(proj_length, abs(track.Length() * dir.X()));
@@ -291,6 +394,8 @@ void sbn::TrackAreaHit::produce(art::Event& e)
 	
 	// add in the window from signal shaping [ticks]
 	window += fSignalShapingWindow;
+	// And longitudinal Diffsion
+        window += (fLongitudinalDiffusionScale*sqrt(2*tdrift*g4param->LongitudinalDiffusion()) / dprop.DriftVelocity()) / dclock.TPCClock().TickPeriod();
 	
 	// convert to a TDCTick range
 	raw::TDCtick_t startTick = std::nearbyint(std::floor(time - window / 2.));
@@ -302,17 +407,48 @@ void sbn::TrackAreaHit::produce(art::Event& e)
           geo::WireID wireID {planeID, (geo::WireID::WireID_t) wire};
           lastWire = wire;
 
-          // First find the corresponding recob::Wire
-          int wire_ind = -1;
-          for (unsigned i_rwire = 0; i_rwire < wires.size(); i_rwire++) {
-            if (geo->PlaneWireToChannel(wireID) == wires[i_rwire]->Channel()) {
-              wire_ind = (int)i_rwire;
-              break;
+          // Get the channel of this wire
+          unsigned channel = geo->PlaneWireToChannel(wireID);
+
+          // If we have already seen this channel, ignore it.
+          //
+          // This can happen in ICARUS because TPC channels are duplicated across 
+          // multiple "logical" TPC's in planes 1 and 2. We don't want to 
+          // double-count the charge on those channels
+          if (channels.count(channel)) continue;
+
+          channels.insert(channel);
+
+          // Make the hit -- using wires or digits
+
+          recob::Hit thisHit;
+          if (fUseWires) {
+            // First find the corresponding recob::Wire
+            int wire_ind = -1;
+            for (unsigned i_rwire = 0; i_rwire < wires.size(); i_rwire++) {
+              if (channel == wires[i_rwire]->Channel()) {
+                wire_ind = (int)i_rwire;
+                break;
+              }
             }
+
+            // make the hit
+            thisHit = MakeHit(*wires[wire_ind], wireID, geo, startTick, endTick);
+          }
+          else {
+            // Find the digits
+            int digit_ind = -1;
+            for (unsigned i_digit = 0; i_digit < digits.size(); i_digit++) {
+              if (channel == digits.at(i_digit)->Channel()) {
+                digit_ind = (int)i_digit;
+                break;
+              }
+            }
+
+            // Make the hit
+            thisHit = MakeHit(*digits[digit_ind], wireID, geo, startTick, endTick);
           }
 
-          // make the hit
-          recob::Hit thisHit = MakeHit(*wires[wire_ind], wireID, geo, startTick, endTick);
           // if invalid hit, ignore it
           if (thisHit.WireID().Wire == geo::WireID::InvalidID) continue; 
 
@@ -321,10 +457,11 @@ void sbn::TrackAreaHit::produce(art::Event& e)
 
           // save to output data
           outHits->push_back(thisHit);
-          art::Ptr<recob::Hit> thisHitPtr = hitPtrMaker(outHits->size()-1);
+
           // add to the association
+          art::Ptr<recob::Hit> thisHitPtr = hitPtrMaker(outHits->size()-1);
           assn->addSingle(tracks[i], thisHitPtr, thisHitMeta);
-          wireAssn->addSingle(thisHitPtr, wires[wire_ind]);
+          // wireAssn->addSingle(thisHitPtr, wires[wire_ind]);
         }
       } 
     }
@@ -332,7 +469,7 @@ void sbn::TrackAreaHit::produce(art::Event& e)
 
   e.put(std::move(outHits));
   e.put(std::move(assn));
-  e.put(std::move(wireAssn));
+  // e.put(std::move(wireAssn));
 
 }
 
