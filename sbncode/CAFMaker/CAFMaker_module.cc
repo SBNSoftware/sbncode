b613090ad885207930772bf351451c4bd4bc9655
Chris Backhouse <cjbackhouse@gmail.com>
2021-11-30 12:04:23 +0000

    Add another globalTree bail out condition for the case where the list of labels is empty.

--- sbncode/CAFMaker/CAFMaker_module.cc
+++ sbncode/CAFMaker/CAFMaker_module.cc
@@ -394,6 +394,8 @@ void CAFMaker::beginRun(art::Run& run) {
   }
 
 
+  if(fParams.SystWeightLabels().empty()) return; // no need for globalTree
+
   SRGlobal global;
 
   for(const std::string& label: fParams.SystWeightLabels()){
