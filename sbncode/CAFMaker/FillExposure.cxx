SBNSoftware/sbncafmaker

Further updates to resolve comments

sbncafmaker/CAFMaker/FillExposure.cxx
@@ -1,5 +1,5 @@
 
-#include "FillExposure.h"
+#include "sbncafmaker/CAFMaker/FillExposure.h"
 
 namespace caf
 {

SBNSoftware/sbncode

sbncode/CAFMaker/FillExposure.cxx
@@ -1,5 +1,5 @@
 
-#include "FillExposure.h"
+#include "sbncode/CAFMaker/FillExposure.h"
 
 namespace caf
 {