3e3e0b944f0097122b9d1363e8470a05a5d0ec9e
Henry Lay <h.lay@lancaster.ac.uk>
2022-02-18 09:33:10 -0600

    Add function to run alg using incoming cosmic hypothesis

--- sbncode/LArRecoProducer/TrackStoppingChi2Alg.h
+++ sbncode/LArRecoProducer/TrackStoppingChi2Alg.h
@@ -12,6 +12,7 @@
 
 #include "fhiclcpp/ParameterSet.h"
 #include "sbnobj/Common/Reco/StoppingChi2Fit.h"
+#include "sbncode/GeometryTools/TPCGeoAlg.h"
 #include "lardataobj/AnalysisBase/Calorimetry.h"
 
 #include <vector>
@@ -26,12 +27,18 @@ namespace sbn {
 
     StoppingChi2Fit RunFit(const std::vector<float> &dEdxVec, const std::vector<float> &resRangeVec) const;
     
+    // Prepare dE/dx and residual range vectors for fitting assuming pandora's track direction
     StoppingChi2Fit RunFit(const anab::Calorimetry& calo) const;
+
+    // Prepare dE/dx and residual range vectors for fitting assuming incoming cosmic hypothesis
+    StoppingChi2Fit RunFitForCosmicID(const anab::Calorimetry& calo) const;
     
   private:
   
     const float fFitRange, fMaxdEdx;
     const unsigned int fMinHits;
+
+    sbn::TPCGeoAlg fTpcGeo;
 
   };
 }
