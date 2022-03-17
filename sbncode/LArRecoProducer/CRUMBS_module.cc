d70ea5f3d11a1e6884c4dee98aeb8ea88e752c4a
Henry Lay <h.lay@lancaster.ac.uk>
2022-03-17 06:11:11 -0500

    Ensure conversion from float to int is safe

--- sbncode/LArRecoProducer/CRUMBS_module.cc
+++ sbncode/LArRecoProducer/CRUMBS_module.cc
@@ -362,9 +362,9 @@ namespace sbn {
       
 	const float score = fMVAReader->EvaluateMVA(fMVAName);
 
-	CRUMBSResult thisResult(score, tpc_CRFracHitsInLongestTrack, tpc_CRLongestTrackDeflection, tpc_CRLongestTrackDirY, tpc_CRNHitsMax,
-				tpc_NuEigenRatioInSphere, tpc_NuNFinalStatePfos, tpc_NuNHitsTotal, tpc_NuNSpacePointsInSphere, tpc_NuVertexY, 
-				tpc_NuWeightedDirZ, tpc_StoppingChi2CosmicRatio, pds_FMTotalScore, pds_FMPE, pds_FMTime, crt_TrackScore, crt_HitScore, 
+	CRUMBSResult thisResult(score, tpc_CRFracHitsInLongestTrack, tpc_CRLongestTrackDeflection, tpc_CRLongestTrackDirY, std::round(tpc_CRNHitsMax),
+				tpc_NuEigenRatioInSphere, std::round(tpc_NuNFinalStatePfos), std::round(tpc_NuNHitsTotal), std::round(tpc_NuNSpacePointsInSphere), 
+				tpc_NuVertexY, tpc_NuWeightedDirZ, tpc_StoppingChi2CosmicRatio, pds_FMTotalScore, pds_FMPE, pds_FMTime, crt_TrackScore, crt_HitScore, 
 				crt_TrackTime, crt_HitTime);
 
 	resultsVec->push_back(thisResult);
