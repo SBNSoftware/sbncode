044fc6c9932dc5c3724c6d9246d2247ab9cdf6e1
Ryan Howell <rhowell@icarusgpvm02.fnal.gov>
2022-04-06 17:24:16 -0500

    added debugging steps

--- sbncode/CAFMaker/CAFMaker_module.cc
+++ sbncode/CAFMaker/CAFMaker_module.cc
@@ -1345,10 +1345,13 @@ void CAFMaker::produce(art::Event& evt) noexcept {
     //#######################################################
     // Reco objects have assns to the slice PFParticles
     // This depends on the findMany object created above.
+    int counter = 0;
+    std::cout<<"Track assc size:"<<fmTrack.size()<<"  Shower assc size:"<<fmShower.size()<<std::endl;
     for ( size_t iPart = 0; iPart < fmPFPart.size(); ++iPart ) {
       const recob::PFParticle &thisParticle = *fmPFPart[iPart];
-
+      counter++;
       std::vector<art::Ptr<recob::Track>> thisTrack;
+      std::cout<<"Track valid:"<<fmTrack.isValid()<<"  Shower valid:"<<fmShower.isValid()<<std::endl;
       if (fmTrack.isValid()) {
         thisTrack = fmTrack.at(iPart);
       }
@@ -1357,9 +1360,11 @@ void CAFMaker::produce(art::Event& evt) noexcept {
         thisShower = fmShower.at(iPart);
       }
 
+      std::cout<<"Track empty:"<<thisTrack.empty()<<"  Shower empty:"<<thisShower.empty()<<std::endl;
       if (!thisTrack.empty())  { // it has a track!
-        assert(thisTrack.size() == 1);
+        //assert(thisTrack.size() == 1);
         rec.reco.ntrk ++;
+	std::cout<<"************** Track: "<<counter<<std::endl;
         rec.reco.trk.push_back(SRTrack());
 
         // collect all the stuff
@@ -1426,8 +1431,9 @@ void CAFMaker::produce(art::Event& evt) noexcept {
       } // thisTrack exists
 
       if (!thisShower.empty()) { // it has shower!
-        assert(thisShower.size() == 1);
+        //assert(thisShower.size() == 1);
         rec.reco.nshw ++;
+	std::cout<<"************** Shower: "<<counter<<std::endl;
         rec.reco.shw.push_back(SRShower());
         FillShowerVars(*thisShower[0], vertex, fmShowerHit.at(iPart), lar::providerFrom<geo::Geometry>(), producer, rec.reco.shw.back());
 
@@ -1459,7 +1465,6 @@ void CAFMaker::produce(art::Event& evt) noexcept {
         recslc.reco.nshw = recslc.reco.shw.size();
 
       } // thisShower exists
-
       else {}
 
     }// end for pfparts
