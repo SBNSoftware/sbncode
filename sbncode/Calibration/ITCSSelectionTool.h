ab9977ada2229a9a08ae659ad114f10bc25088a9
gputnam <gputnam@icarusgpvm03.fnal.gov>
2022-07-20 10:17:11 -0500

    Make selections 0-2 in the not0 config same as default. Add in selection 3 to scoop up all remaining tracks.

--- sbncode/Calibration/ITCSSelectionTool.h
+++ sbncode/Calibration/ITCSSelectionTool.h
@@ -10,6 +10,7 @@ class ITCSSelectionTool {
 public:
   ITCSSelectionTool(const fhicl::ParameterSet &p):
     fAllowT0(p.get<std::vector<bool>>("AllowT0", {})),
+    fRequireT0(p.get<bool>("RequireT0", true)),
     fInvert(p.get<bool>("Invert", false)),
     fNPreScale(p.get<unsigned>("PreScale", 1)),
     fISelect(0) 
@@ -25,12 +26,15 @@ public:
   /// For external modules to call: run the actual selection
   bool DoSelect(const TrackInfo &t) {
 
+    bool has_good_t0 = t.whicht0 >= 0;
     // Check if the T0 is allowed
     if (!fAllowT0.empty()) {
       if (t.whicht0 >= 0 && ((unsigned)t.whicht0 >= fAllowT0.size() || !fAllowT0[t.whicht0])) {
-        return false;
+        has_good_t0 = false;
       }
     }
+
+    if (!has_good_t0 && fRequireT0) return false;
 
     bool selected = Select(t) == !fInvert /* implement inversion */;
 
@@ -47,6 +51,7 @@ protected:
   virtual bool Select(const TrackInfo &t) = 0;
 
   std::vector<bool> fAllowT0;
+  bool fRequireT0;
   bool fInvert;
   unsigned fNPreScale;
   unsigned fISelect;
