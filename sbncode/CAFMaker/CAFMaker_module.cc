8f9d027735314bb94bb03cf30830c7b2569ea619
rhowell42 <39918054+rhowell42@users.noreply.github.com>
2022-08-30 19:47:16 -0400

    Update sbncode/CAFMaker/CAFMaker_module.cc
    Co-authored-by: Gianluca Petrillo <petrillo@slac.stanford.edu>

--- sbncode/CAFMaker/CAFMaker_module.cc
+++ sbncode/CAFMaker/CAFMaker_module.cc
@@ -1281,10 +1281,8 @@ void CAFMaker::produce(art::Event& evt) noexcept {
       assert(thisParticle.size() == 1);
      
       SRPFP pfp;
-      // ...
-      rec.reco.pfp.push_back(std::move(pfp));
       const larpandoraobj::PFParticleMetadata *pfpMeta = (fmPFPMeta.at(iPart).empty()) ? NULL : fmPFPMeta.at(iPart).at(0).get();
-      FillPFPVars(thisParticle, primary, pfpMeta, rec.reco.pfp.back());
+      FillPFPVars(thisParticle, primary, pfpMeta, pfp);
 
       if (!thisTrack.empty())  { // it has a track!
         assert(thisTrack.size() == 1);
