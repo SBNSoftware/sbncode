5f6bcfcfae81c2cd358f680f69d799580452c582
Elizabeth Worcester <etw@bnl.gov>
2022-09-08 13:45:02 -0500

    tidy up variable declaration

--- sbncode/CAFMaker/CAFMaker_module.cc
+++ sbncode/CAFMaker/CAFMaker_module.cc
@@ -1748,8 +1748,7 @@ void CAFMaker::produce(art::Event& evt) noexcept {
     }
 
     //Generate random number to decide if event is saved in prescale or blinded file
-    bool keepprescale;
-    keepprescale = fBlindTRandom->Uniform() < 1/fParams.PrescaleFactor();
+    const bool keepprescale = fBlindTRandom->Uniform() < 1/fParams.PrescaleFactor();
     if(fRecTreeb || fRecTreep) {
       rec.hdr.evt = 0;
       if (keepprescale) {
