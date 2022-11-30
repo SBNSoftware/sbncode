403a4e431abeb969111a8331716a1cba028e1de3
gputnam <gputnam@icarusgpvm03.fnal.gov>
2022-11-30 10:55:57 -0600

    Remove unused variables.

--- sbncode/CAFMaker/FillTrigger.cxx
+++ sbncode/CAFMaker/FillTrigger.cxx
@@ -7,12 +7,8 @@ namespace caf
 		   const raw::Trigger& trig,
 		   caf::SRTrigger& triggerInfo)
   {
-    uint64_t beam_ts = 0;
-    uint64_t trigger_ts = 0;
     triggerInfo.global_trigger_time = addltrig_info.triggerTimestamp;
     triggerInfo.beam_gate_time_abs = addltrig_info.beamGateTimestamp;
-    beam_ts = addltrig_info.beamGateTimestamp;
-    trigger_ts = addltrig_info.triggerTimestamp;
     triggerInfo.beam_gate_det_time = trig.BeamGateTime();
     triggerInfo.global_trigger_det_time = trig.TriggerTime();
     double diff_ts = triggerInfo.global_trigger_det_time - triggerInfo.beam_gate_det_time;
