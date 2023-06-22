507e40407fa56f77cdbfcf4c105c885bdbe39c72
Robert Darby <rdarby@sbndgpvm02.fnal.gov>
2023-06-22 15:16:20 -0500

    Added other slices to CAFMaker

--- sbncode/CAFMaker/FillFlashMatch.h
+++ sbncode/CAFMaker/FillFlashMatch.h
@@ -29,44 +29,44 @@ namespace caf
   void FillSliceFlashMatchB(const sbn::SimpleFlashMatch* fmatch,
                             caf::SRSlice& srslice,
                             bool allowEmpty = false);
+
+
+  void FillSliceFlashMatchOp(const sbn::SimpleFlashMatch* fmatchop,
+                           caf::SRSlice& srslice,
+                           bool allowEmpty = false);
+
+  void FillSliceFlashMatchOpA(const sbn::SimpleFlashMatch* fmatchop,
+                            caf::SRSlice& srslice,
+                            bool allowEmpty = false);
+
+  void FillSliceFlashMatchOpB(const sbn::SimpleFlashMatch* fmatchop,
+                            caf::SRSlice& srslice,
+                            bool allowEmpty = false);
+
+
+  void FillSliceFlashMatchARA(const sbn::SimpleFlashMatch* fmatchara,
+                           caf::SRSlice& srslice,
+                           bool allowEmpty = false);
+
+  void FillSliceFlashMatchARAA(const sbn::SimpleFlashMatch* fmatchara,
+                            caf::SRSlice& srslice,
+                            bool allowEmpty = false);
+
+  void FillSliceFlashMatchARAB(const sbn::SimpleFlashMatch* fmatchara,
+                            caf::SRSlice& srslice,
+                            bool allowEmpty = false);
+
+
+  void FillSliceFlashMatchOpARA(const sbn::SimpleFlashMatch* fmatchopara,
+                           caf::SRSlice& srslice,
+                           bool allowEmpty = false);
+
+  void FillSliceFlashMatchOpARAA(const sbn::SimpleFlashMatch* fmatchopara,
+                            caf::SRSlice& srslice,
+                            bool allowEmpty = false);
+
+  void FillSliceFlashMatchOpARAB(const sbn::SimpleFlashMatch* fmatchopara,
+                            caf::SRSlice& srslice,
+                            bool allowEmpty = false);
 }
-
-  void FillSliceFlashMatchOp(const sbn::SimpleFlashMatch* fmatch,
-                           caf::SRSlice& srslice,
-                           bool allowEmpty = false);
-
-  void FillSliceFlashMatchOpA(const sbn::SimpleFlashMatch* fmatch,
-                            caf::SRSlice& srslice,
-                            bool allowEmpty = false);
-
-  void FillSliceFlashMatchOpB(const sbn::SimpleFlashMatch* fmatch,
-                            caf::SRSlice& srslice,
-                            bool allowEmpty = false);
-
-
-  void FillSliceFlashMatchARA(const sbn::SimpleFlashMatch* fmatch,
-                           caf::SRSlice& srslice,
-                           bool allowEmpty = false);
-
-  void FillSliceFlashMatchARAA(const sbn::SimpleFlashMatch* fmatch,
-                            caf::SRSlice& srslice,
-                            bool allowEmpty = false);
-
-  void FillSliceFlashMatchARAB(const sbn::SimpleFlashMatch* fmatch,
-                            caf::SRSlice& srslice,
-                            bool allowEmpty = false);
-
-
-  void FillSliceFlashMatchOpARA(const sbn::SimpleFlashMatch* fmatch,
-                           caf::SRSlice& srslice,
-                           bool allowEmpty = false);
-
-  void FillSliceFlashMatchOpARAA(const sbn::SimpleFlashMatch* fmatch,
-                            caf::SRSlice& srslice,
-                            bool allowEmpty = false);
-
-  void FillSliceFlashMatchOpARAB(const sbn::SimpleFlashMatch* fmatch,
-                            caf::SRSlice& srslice,
-                            bool allowEmpty = false);
-
 #endif
