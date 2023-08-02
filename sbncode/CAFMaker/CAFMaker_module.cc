9712afa6f7021d07bbdb7983f56d5527849267f9
Gianluca Petrillo <petrillo@slac.stanford.edu>
2023-08-02 13:35:17 -0500

    CAFMaker adapted for SRHeader workaround
    This commit should NOT end in the development branch (REVERT it there).

--- sbncode/CAFMaker/CAFMaker_module.cc
+++ sbncode/CAFMaker/CAFMaker_module.cc
@@ -180,7 +180,7 @@ class CAFMaker : public art::EDProducer {
   std::string fSourceFile;
 
   bool fFirstInSubRun;
-  unsigned int fIndexInFile = SRHeader::NoSourceIndex;
+  unsigned int fIndexInFile = SRHeader::NoSourceIndex();
   bool fFirstBlindInFile;
   bool fFirstPrescaleInFile;
   int fFileNumber;
@@ -929,7 +929,7 @@ void CAFMaker::InitializeOutfiles()
   fTotalEvents = 0;
   fBlindEvents = 0;
   fPrescaleEvents = 0;
-  fIndexInFile = SRHeader::NoSourceIndex;
+  fIndexInFile = SRHeader::NoSourceIndex();
   fFirstInSubRun = false;
   // fCycle = -5;
   // fBatch = -5;
