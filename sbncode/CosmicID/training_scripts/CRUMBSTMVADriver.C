d6c7b6573e2dd90311258be38e528b7342cf78d4
Henry Lay <h.lay@lancaster.ac.uk>
2024-01-19 11:30:07 -0600

    Import first draft of CRUMBS OpT0

--- sbncode/CosmicID/training_scripts/CRUMBSTMVADriver.C
+++ sbncode/CosmicID/training_scripts/CRUMBSTMVADriver.C
@@ -14,26 +14,31 @@
 
 void TrainCRUMBSInstance(const TString outDirName, TTree *inputTree, 
                          const TCut sigCut, const TCut backCut,
+                         const bool useOpT0Finder = true, const bool useSimpleFlash = false,
                          const double signalWeight = 1.0, const double backgroundWeight = 1.0);
-int CRUMBSTMVADriver()
+
+int CRUMBSTMVADriver(const bool useOpT0Finder = true, const bool useSimpleFlash = false)
 {
+  if(useOpT0Finder && useSimpleFlash)
+    {
+      std::cout << "Cannot use both flash matchers, please chose one..." << std::endl;
+      return 0;
+    }
+  else if(!useOpT0Finder && !useSimpleFlash)
+    {
+      std::cout << "Would recommend using one of the flash matchers..." << std::endl;
+      return 0;
+    }
+
   TMVA::Tools::Instance();
 
   std::cout << std::endl;
   std::cout << "==> Start CRUMBS TMVA Training" << std::endl;
 
-  TFile *input(0);
-  TString fname = "PATH/TO/YOUR/TREE/FILE/HERE";
-  if (!gSystem->AccessPathName( fname )) {
-    input = TFile::Open( fname );
-  }
-  if (!input) {
-    std::cout << "ERROR: could not open data file" << std::endl;
-    exit(1);
-  }
-  std::cout << "--- CRUMBS TMVA Training     : Using input file: " << input->GetName() << std::endl;
-
-  TTree *inputTree     = (TTree*)input->Get("crumbs/SliceTree");
+  TChain *inputTree = new TChain("crumbs/SliceTree");
+  inputTree->Add("/ADD/WHATEVER/SAMPLES/YOU'RE/USING/HERE");
+  // Recommend using a combination of a rockbox sample, an intrinsic electron neutrino sample
+  // and an intime cosmics sample in roughly a 2:2:1 ratio
 
   if (!inputTree) {
     std::cout << "ERROR: could not access tree" << std::endl;
@@ -47,10 +52,10 @@ int CRUMBSTMVADriver()
   TCut ccnueSignal     = inclusiveSignal + "ccnc == 0 && abs(nutype) == 12";
   TCut ncSignal        = inclusiveSignal + "ccnc == 1";
 
-  TrainCRUMBSInstance("CRUMBS_Inclusive", inputTree, inclusiveSignal, background);
-  TrainCRUMBSInstance("CRUMBS_CCNuMu", inputTree, ccnumuSignal, background);
-  TrainCRUMBSInstance("CRUMBS_CCNuE", inputTree, ccnueSignal, background);
-  TrainCRUMBSInstance("CRUMBS_NC", inputTree, ncSignal, background);
+  TrainCRUMBSInstance("CRUMBS_Inclusive", inputTree, inclusiveSignal, background, useOpT0Finder, useSimpleFlash);
+  TrainCRUMBSInstance("CRUMBS_CCNuMu", inputTree, ccnumuSignal, background, useOpT0Finder, useSimpleFlash);
+  TrainCRUMBSInstance("CRUMBS_CCNuE", inputTree, ccnueSignal, background, useOpT0Finder, useSimpleFlash);
+  TrainCRUMBSInstance("CRUMBS_NC", inputTree, ncSignal, background, useOpT0Finder, useSimpleFlash);
 
   std::cout << std::endl;
   std::cout << "==> Finish CRUMBS TMVA Training" << std::endl;
@@ -60,6 +65,7 @@ int CRUMBSTMVADriver()
 
 void TrainCRUMBSInstance(const TString outDirName, TTree *inputTree, 
                          const TCut sigCut, const TCut backCut,
+                         const bool useOpT0Finder, const bool useSimpleFlash,
                          const double signalWeight, const double backgroundWeight)
 {
   std::cout << std::endl;
@@ -72,7 +78,7 @@ void TrainCRUMBSInstance(const TString outDirName, TTree *inputTree,
   TFile* outputFile = TFile::Open( outfileName, "RECREATE" );
 
   TMVA::Factory *factory = new TMVA::Factory( "CrumbsTMVAClassification", outputFile,
-                                              "!V:!Silent:Color:DrawProgressBar:Transformations=I;D;P;G,D:AnalysisType=Classification" );
+                                              "!V:!Silent:Color:DrawProgressBar:AnalysisType=Classification" );
 
   dataloader->AddVariable("tpc_CRFracHitsInLongestTrack","Fraction of Hits in Longest Track (Cosmic Reco)","",'F');
   dataloader->AddVariable("tpc_CRLongestTrackDeflection","Longest Track Deflection (Cosmic Reco)","",'F');
@@ -86,9 +92,18 @@ void TrainCRUMBSInstance(const TString outDirName, TTree *inputTree,
   dataloader->AddVariable("tpc_NuWeightedDirZ","Weighted Z Direction (Nu Reco)","",'F');
   dataloader->AddVariable("tpc_StoppingChi2CosmicRatio","Stopping Fits Chi2 Ratio","",'F');
 
-  dataloader->AddVariable("pds_FMTotalScore","Total FM Score","",'F');
-  dataloader->AddVariable("pds_FMPE","nPE in flash","",'F');
-  dataloader->AddVariable("pds_FMTime","FM Time","#mu s",'F');
+  if(useOpT0Finder)
+    {
+      dataloader->AddVariable("pds_OpT0Score","OpT0 Score","",'F');
+      dataloader->AddVariable("isinf(pds_OpT0MeasuredPE) ? -10000 : pds_OpT0MeasuredPE", "OpT0 Measured PE","",'F');
+    }
+
+  if(useSimpleFlash)
+    {
+      dataloader->AddVariable("pds_FMTotalScore","Total FM Score","",'F');
+      dataloader->AddVariable("pds_FMPE","nPE in flash","",'F');
+      dataloader->AddVariable("pds_FMTime","FM Time","#mu s",'F');
+    }
 
   dataloader->AddVariable("crt_TrackScore","CRT Track Match Score","",'F');
   dataloader->AddVariable("crt_SPScore","CRT SpacePoint Match Score","",'F');
@@ -101,7 +116,7 @@ void TrainCRUMBSInstance(const TString outDirName, TTree *inputTree,
   dataloader->PrepareTrainingAndTestTree( sigCut, backCut, "SplitMode=random:!V" );
   
   factory->BookMethod( dataloader, TMVA::Types::kBDT, "BDT",
-                       "!H:!V:NTrees=850:MinNodeSize=2.5%:MaxDepth=3:BoostType=AdaBoost:AdaBoostBeta=0.5:UseBaggedBoost:BaggedSampleFraction=0.5:SeparationType=GiniIndex:nCuts=20");
+                       "!H:!V:NTrees=100:MinNodeSize=2.5%:MaxDepth=3:BoostType=AdaBoost:AdaBoostBeta=0.5:UseBaggedBoost:BaggedSampleFraction=0.5:SeparationType=GiniIndex:nCuts=100");
 
   factory->TrainAllMethods();
   
