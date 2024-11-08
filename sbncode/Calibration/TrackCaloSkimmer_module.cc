4356ba65d3ec3467b24aa417309751d3917f90c0
Giovanni Chiello <giovanni.chiello.gc@gmail.com>
2024-11-08 09:26:55 -0600

    added mcs_angles for both final and initial cut for both algoritms

--- sbncode/Calibration/TrackCaloSkimmer_module.cc
+++ sbncode/Calibration/TrackCaloSkimmer_module.cc
@@ -1150,16 +1150,20 @@ float minLen=40;
     fTrack->mcs_momentum_icarusIC = mcsIIC->fwdMomentum();
     fTrack->d3p_icarusIC = mcsIIC->bwdLogLikelihood();
 
+    for(size_t ja=0;ja<mcsUFC->scatterAngles().size();ja++)
+      fTrack->mcs_angles_ubooneFC.push_back(mcsUFC->scatterAngles().at(ja));
     for(size_t ja=0;ja<mcsUIC->scatterAngles().size();ja++)
-      fTrack->MCSangles_uboone.push_back(mcsUIC->scatterAngles().at(ja));
+      fTrack->mcs_angles_ubooneIC.push_back(mcsUIC->scatterAngles().at(ja));
+    for(size_t ja=0;ja<mcsIFC->scatterAngles().size();ja++)
+      fTrack->mcs_angles_icarusFC.push_back(mcsIFC->scatterAngles().at(ja));
     for(size_t ja=0;ja<mcsIIC->scatterAngles().size();ja++)
-      fTrack->MCSangles_icarus.push_back(mcsIIC->scatterAngles().at(ja));
-    for(size_t ja=0;ja<mcsUIC->scatterAngles().size();ja++)
-      fTrack->seglens_uboone.push_back(mcsUIC->scatterAngles().at(ja));
-    for(size_t ja=0;ja<mcsIIC->scatterAngles().size();ja++)
-      fTrack->seglens_icarus.push_back(mcsIIC->scatterAngles().at(ja));
-    for(size_t ja=0;ja<mcsIIC->segmentRadLengths().size();ja++)
-      fTrack->tt600_icarus.push_back(mcsIIC->segmentRadLengths().at(ja));
+      fTrack->mcs_angles_icarusIC.push_back(mcsIIC->scatterAngles().at(ja));
+    for(size_t ja=0;ja<mcsUFC->scatterAngles().size();ja++)
+      fTrack->seglens_uboone.push_back(mcsUFC->scatterAngles().at(ja));
+    for(size_t ja=0;ja<mcsIFC->scatterAngles().size();ja++)
+      fTrack->seglens_icarus.push_back(mcsIFC->scatterAngles().at(ja));
+    for(size_t ja=0;ja<mcsIFC->segmentRadLengths().size();ja++)
+      fTrack->tt600_icarus.push_back(mcsIFC->segmentRadLengths().at(ja));
    if( fTrack->length>minLen) {
      // if(mcsU->fwdMomentum()>0.) std::cout << " mcs momentum uboone " <<  mcsU->fwdMomentum() << std::endl;
      //if(mcsI->fwdMomentum()>0.)std::cout << " mcs momentum icarus " <<  mcsI->fwdMomentum() << std::endl;
