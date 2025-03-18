f1a7e632bfabb43aca5f2b4a717bcefb248a2e23
Nathaniel Rowe <nrowe@sbndbuild03.fnal.gov>
2025-03-18 13:30:25 -0500

    remove newly-duplicate code in icarus

--- sbncode/BeamSpillInfoRetriever/ICARUSBNBRetriever/ICARUSBNBRetriever_module.cc
+++ sbncode/BeamSpillInfoRetriever/ICARUSBNBRetriever/ICARUSBNBRetriever_module.cc
@@ -10,33 +10,12 @@
  */
 
 #include "art/Framework/Core/EDProducer.h"
-#include "art/Framework/Core/ModuleMacros.h"
-#include "art/Framework/Principal/Event.h"
-#include "art/Framework/Principal/Handle.h"
-#include "art/Framework/Principal/Run.h"
-#include "art/Framework/Principal/SubRun.h"
-#include "canvas/Utilities/InputTag.h"
-#include "fhiclcpp/types/Atom.h"
 #include "messagefacility/MessageLogger/MessageLogger.h"
-#include "larcorealg/CoreUtils/counter.h"
-
-#include "artdaq-core/Data/Fragment.hh"
-#include "sbndaq-artdaq-core/Overlays/ICARUS/ICARUSTriggerV3Fragment.hh"
-#include "sbnobj/Common/Trigger/ExtraTriggerInfo.h"
-
 #include "sbnobj/Common/POTAccounting/BNBSpillInfo.h"
-
-#include "ifdh_art/IFBeamService/IFBeam_service.h"
-#include "ifbeam_c.h"
-#include "MWRData.h"
-
-#include <memory>
-#include <vector>
-#include <cassert>
-
+#include "sbncode/BeamSpillInfoRetriever/SBNDPOTTools.h"
+#include "sbnobj/Common/Trigger/ExtraTriggerInfo.h"
+#include "sbndaq-artdaq-core/Overlays/ICARUS/ICARUSTriggerV3Fragment.hh"
 #include <sqlite3.h>
-#include <cstdio>
-#include <sstream>
 
 namespace sbn {
   class ICARUSBNBRetriever;
@@ -55,7 +34,13 @@ public:
       Comment{ "extension to the time window considered when collecting spills [seconds]" },
       0.0333 // default
       };
-    
+ 
+    fhicl::Atom<double> BESOffset {
+      Name{ "BESOffset" },
+      Comment{ "Offset between beam early warning signal and $1D [seconds]" },
+      0.036 // default
+      };
+   
     fhicl::Atom<std::string> RawDataLabel {
       Name{ "raw_data_label" },
       Comment{ "art data product instance name for trigger information (product label is 'daq')" }
@@ -138,32 +123,10 @@ private:
   sqlite3 *db;
   int rc;
 
-  struct TriggerInfo_t {
-    int gate_type = 0; ///< Source of the spill: `1`: BNB, `2`: NuMI
-    double t_current_event  = 0;
-    double t_previous_event = 0;
-    unsigned int number_of_gates_since_previous_event = 0; // FIXME needs to be integral type
-    std::int64_t WR_to_Spill_conversion = 0;
-  };
-  
-  struct MWRdata_t {
-    std::vector< std::vector<double> > MWR_times;
-    std::vector< std::vector< std::vector< int > > > unpacked_MWR;
-  };
-  
-
   static constexpr double MWRtoroidDelay = -0.035; ///< the same time point is measured _t_ by MWR and _t + MWRtoroidDelay`_ by the toroid [ms]
 
   /// Returns the information of the trigger in the current event.
   TriggerInfo_t extractTriggerInfo(art::Event const& e) const;
-  
-  /**
-   * @brief Determines spill times and extracts data based on multiwire devices.
-   * @param triggerInfo information from the trigger of this event
-   * @return times and unpacked data, per device (`"M875BB"`, `"M876BB"`, `"MMBTBB"`)
-   */
-  MWRdata_t extractSpillTimes(TriggerInfo_t const& triggerInfo) const;
-
   /**
    * @brief Matches spill times with multiwire chamber data from the database.
    * @param eventID ID of the event the information is associated to
@@ -179,17 +142,6 @@ private:
     MWRdata_t const& MWRdata, bool isFirstEventInRun,
     std::vector< sbn::BNBSpillInfo >& beamInfos
     ) const;
-
-  /**
-   * @brief Assembles and returns a spill information record.
-   * @param eventID ID of the event the information is associated to
-   * @param time time of the spill
-   * @param MWRdata all extracted data from multiwire chambers
-   * @param matched_MWR data from multiwire chambers matched with the time
-   * @return a `sbn::BNBSpillInfo` object with information on the spill at `time`
-   */
-  sbn::BNBSpillInfo makeBNBSpillInfo
-    (art::EventID const& eventID, double time, MWRdata_t const& MWRdata, std::vector<int> const& matched_MWR) const;
 
 /**
  * @brief SQLite callback function for retrieving trigger_type from a query.
@@ -323,13 +275,9 @@ void sbn::ICARUSBNBRetriever::produce(art::Event& e)
   // Keep track of the number of beam gates the DAQ thinks 
   //   are in this job
   TotalBeamSpills += triggerInfo.number_of_gates_since_previous_event;
-  
-  
-  MWRdata_t const MWRdata = extractSpillTimes(triggerInfo);
-  
+  MWRdata_t const MWRdata = extractSpillTimes(triggerInfo, bfp, bfp_mwr, fTimePad, MWRtoroidDelay, mwrdata);
   
   int const spill_count = matchMultiWireData(e.id(), triggerInfo, MWRdata, e.event() == 1, fOutbeamInfos);
-  
   
   if(spill_count > int(triggerInfo.number_of_gates_since_previous_event))
     mf::LogDebug("ICARUSBNBRetriever")<< "Event Spills : " << spill_count << ", DAQ Spills : " << triggerInfo.number_of_gates_since_previous_event << " \t \t ::: WRONG!"<< std::endl;
@@ -339,7 +287,7 @@ void sbn::ICARUSBNBRetriever::produce(art::Event& e)
 }//end iteration over art::Events
 
 
-sbn::ICARUSBNBRetriever::TriggerInfo_t sbn::ICARUSBNBRetriever::extractTriggerInfo(art::Event const& e) const {
+sbn::TriggerInfo_t sbn::ICARUSBNBRetriever::extractTriggerInfo(art::Event const& e) const {
   
   //Here we read in the artdaq Fragments and extract three pieces of information:
   // 1. The time of the current event, t_current_event
@@ -385,123 +333,6 @@ sbn::ICARUSBNBRetriever::TriggerInfo_t sbn::ICARUSBNBRetriever::extractTriggerIn
 
   return triggerInfo;
 }
-
-
-sbn::ICARUSBNBRetriever::MWRdata_t sbn::ICARUSBNBRetriever::extractSpillTimes(TriggerInfo_t const& triggerInfo) const {
-  
-  // These lines get everything primed within the IFBeamDB
-  //   They seem redundant but they are needed
-  try{bfp->FillCache((triggerInfo.t_previous_event)-fTimePad);} catch (WebAPIException &we) {}      
-  try{bfp_mwr->FillCache((triggerInfo.t_previous_event)-fTimePad);} catch (WebAPIException &we) {}
-  
-  //The multiwire chambers provide their
-  // data in a vector format but we'll have 
-  // to sort through it in std::string format
-  // to correctly unpack it
-  std::vector< std::vector< std::vector< int > > >  unpacked_MWR;
-  std::vector< std::vector< double> > MWR_times;
-  unpacked_MWR.resize(3);
-  MWR_times.resize(3);
-  std::string packed_data_str; 
-  
-  //Create a list of all the MWR devices with their different
-  // memory buffer increments 
-  // generally in the format: "E:<Device>.{Memory Block}"
-  std::vector<std::string> vars = bfp_mwr->GetDeviceList();
-  mf::LogDebug("ICARUSBNBRetriever") << " Number of MWR Device Blocks Found : " << vars.size() << std::endl;
-  // Tracking the time from the IFBeamDB
-  double time_for_mwr;    
-  
-  // this is an iterator to track which of the 
-  // three devices we will be working with
-  int dev = 0;
-  
-  // The MWR devices are annoying and have confusing buffer
-  // what we'll do is sort through all of them first and then 
-  // match them to the closest spills in time
-  // 
-
-  //  int t_steps = int(((triggerInfo.t_previous_event - fTimePad) - (triggerInfo.t_current_event + fTimePad))/0.5)+25;
-  int t_steps = int(((triggerInfo.t_current_event + fTimePad) - (triggerInfo.t_previous_event - fTimePad - 20.))/0.5)+25;
-  mf::LogDebug("ICARUSBNBRetriever") << " t_steps " << t_steps << std::endl;
-
-  for(int t = 0; t < t_steps; t++){//Iterate through time increments
-    for (std::string const& var : vars) {// Iterate through the devices
-      
-      //Make sure we have a device
-      if(var.empty()){ 
-	//mf::LogDebug("ICARUSBNBRetriever") << " NO MWR DEVICES?!" << std::endl;
-	continue;
-      }
-      /// Check the device name and interate the double-vector index
-      if(var.find("M875BB") != std::string::npos ) dev = 0;
-      else if(var.find("M876BB") != std::string::npos ) dev = 1;
-      else if(var.find("MMBTBB") != std::string::npos ) dev = 2;
-      else{
-	//mf::LogDebug("ICARUSBNBRetriever") << " NOT matched to a MWR DEVICES?!" << var << std::endl;
-	continue;}
-      
-      time_for_mwr = 0;
-      
-      try{
-	//Pull the MWR data for the device
-	// these data are "packed"
-	std::vector<double> packed_MWR = bfp_mwr->GetNamedVector((triggerInfo.t_previous_event)-20.-fTimePad+double(0.5*t),var,&time_for_mwr);
-
-	//We'll convert this into a format
-	// that we can unpack doubles >> strings
-	//
-	packed_data_str.clear();
-	packed_data_str += std::to_string(int(time_for_mwr));
-	packed_data_str.append(",");
-	packed_data_str.append(var);
-	packed_data_str.append(",,");
-	
-	/*	for(auto const value: packed_MWR){
-	  packed_data_str += ',';
-	  packed_data_str += std::to_string(int(value));
-	  }*/
-	for(int j = 0; j < int(packed_MWR.size()); j++){
-	  packed_data_str += std::to_string(int(packed_MWR[j]));
-	  if(j < int(packed_MWR.size())-1)
-	    packed_data_str.append(",");
-	}
-	
-	// Use Zarko's unpacking function to turn this into consumeable data
-	std::vector<double> MWR_times_temp;
-	
-	// There is a 35 ms offset between the toriod and the MWR times
-	//   we'll just remove that here to match to the spill times
-	std::vector< std::vector< int > > unpacked_MWR_temp = mwrdata.unpackMWR(packed_data_str,MWR_times_temp,MWRtoroidDelay);
-	
-	//There are four events that are packed into one MWR IFBeam entry
-	for(std::size_t s: util::counter(unpacked_MWR_temp.size())){
-	  	  
-	  // If this entry has a unique time them store it for later	  
-	  if(std::find(MWR_times[dev].begin(), MWR_times[dev].end(), MWR_times_temp[s]) == MWR_times[dev].end()){
-	    unpacked_MWR[dev].push_back(unpacked_MWR_temp[s]);
-	    MWR_times[dev].push_back(MWR_times_temp[s]);
-	  }//check for unique time 
-	}//Iterate through the unpacked events
-	}//try
-      catch (WebAPIException &we) {
-	//Ignore when we can't find the MWR devices
-	//   they don't always report and the timing of them can be annoying
-             
-	}//catch
-    }// Iterate over all the multiwire devices
-  }// Iterate over all times
-
-  mf::LogDebug("ICARUSBNBRetriever") << " Number of MWR[0] times : " << MWR_times[0].size() << std::endl;	
-  mf::LogDebug("ICARUSBNBRetriever") << " Number of MWR[0]s : " << unpacked_MWR[0].size() << std::endl;	
-  mf::LogDebug("ICARUSBNBRetriever") << " Number of MWR[1] times : " << MWR_times[1].size() << std::endl;	
-  mf::LogDebug("ICARUSBNBRetriever") << " Number of MWR[1]s : " << unpacked_MWR[1].size() << std::endl;	
-  mf::LogDebug("ICARUSBNBRetriever") << " Number of MWR[2] times : " << MWR_times[2].size() << std::endl;	
-  mf::LogDebug("ICARUSBNBRetriever") << " Number of MWR[2]s : " << unpacked_MWR[2].size() << std::endl;	
-  
-  return { std::move(MWR_times), std::move(unpacked_MWR) };
-}
-
 
 int sbn::ICARUSBNBRetriever::matchMultiWireData(
   art::EventID const& eventID,
@@ -636,7 +467,7 @@ int sbn::ICARUSBNBRetriever::matchMultiWireData(
       
     }//end loop over MWR devices
     
-    sbn::BNBSpillInfo spillInfo = makeBNBSpillInfo(eventID, times_temps[i], MWRdata, matched_MWR);
+    sbn::BNBSpillInfo spillInfo = makeBNBSpillInfo(eventID, times_temps[i], MWRdata, matched_MWR, bfp);
 
     beamInfos.push_back(std::move(spillInfo));
 
@@ -650,113 +481,6 @@ int sbn::ICARUSBNBRetriever::matchMultiWireData(
 
   return spill_count;
 }
-
-
-sbn::BNBSpillInfo sbn::ICARUSBNBRetriever::makeBNBSpillInfo
-  (art::EventID const& eventID, double time, MWRdata_t const& MWRdata, std::vector<int> const& matched_MWR) const
-{
-  
-  auto const& [ MWR_times, unpacked_MWR ] = MWRdata; // alias
-  
-  // initializing all of our device carriers
-  // device definitions can be found in BNBSpillInfo.h
-  
-  double TOR860 = 0; // units e12 protons
-  double TOR875 = 0; // units e12 protons
-  double LM875A = 0; // units R/s
-  double LM875B = 0; // units R/s
-  double LM875C = 0; // units R/s
-  double HP875 = 0; // units mm
-  double VP875 = 0; // units mm
-  double HPTG1 = 0; // units mm
-  double VPTG1 = 0; // units mm
-  double HPTG2 = 0; // units mm
-  double VPTG2 = 0; // units mm
-  double BTJT2 = 0; // units Deg C
-  double THCURR = 0; // units kiloAmps
-  
-  double TOR860_time = 0; // units s
-    
-  // Here we request all the devices
-  // since sometimes devices fail to report we'll
-  // allow each to throw an exception but still move forward
-  // interpreting these failures will be part of the beam quality analyses 
-  try{bfp->GetNamedData(time, "E:TOR860@",&TOR860,&TOR860_time);}catch (WebAPIException &we) {mf::LogDebug("ICARUSBNBRetriever")<< "At time : " << time << " " << "got exception: " << we.what() << "\n";}
-  try{bfp->GetNamedData(time, "E:TOR875",&TOR875);}catch (WebAPIException &we) {mf::LogDebug("ICARUSBNBRetriever")<< "At time : " << time << " " << "got exception: " << we.what() << "\n";}
-  try{bfp->GetNamedData(time, "E:LM875A",&LM875A);}catch (WebAPIException &we) {mf::LogDebug("ICARUSBNBRetriever")<< "At time : " << time << " " << "got exception: " << we.what() << "\n";}
-  try{bfp->GetNamedData(time, "E:LM875B",&LM875B);}catch (WebAPIException &we) {mf::LogDebug("ICARUSBNBRetriever")<< "At time : " << time << " " << "got exception: " << we.what() << "\n";}
-  try{bfp->GetNamedData(time, "E:LM875C",&LM875C);}catch (WebAPIException &we) {mf::LogDebug("ICARUSBNBRetriever")<< "At time : " << time << " " << "got exception: " << we.what() << "\n";}
-  try{bfp->GetNamedData(time, "E:HP875",&HP875);}catch (WebAPIException &we) {mf::LogDebug("ICARUSBNBRetriever")<< "At time : " << time << " " << "got exception: " << we.what() << "\n";}
-  try{bfp->GetNamedData(time, "E:VP875",&VP875);}catch (WebAPIException &we) {mf::LogDebug("ICARUSBNBRetriever")<< "At time : " << time << " " << "got exception: " << we.what() << "\n";}
-  try{bfp->GetNamedData(time, "E:HPTG1",&HPTG1);}catch (WebAPIException &we) {mf::LogDebug("ICARUSBNBRetriever")<< "At time : " << time << " " << "got exception: " << we.what() << "\n";}
-  try{bfp->GetNamedData(time, "E:VPTG1",&VPTG1);}catch (WebAPIException &we) {mf::LogDebug("ICARUSBNBRetriever")<< "At time : " << time << " " << "got exception: " << we.what() << "\n";}
-  try{bfp->GetNamedData(time, "E:HPTG2",&HPTG2);}catch (WebAPIException &we) {mf::LogDebug("ICARUSBNBRetriever")<< "At time : " << time << " " << "got exception: " << we.what() << "\n";}
-  try{bfp->GetNamedData(time, "E:VPTG2",&VPTG2);}catch (WebAPIException &we) {mf::LogDebug("ICARUSBNBRetriever")<< "At time : " << time << " " << "got exception: " << we.what() << "\n";}
-  try{bfp->GetNamedData(time, "E:BTJT2",&BTJT2);}catch (WebAPIException &we) {mf::LogDebug("ICARUSBNBRetriever")<< "At time : " << time << " " << "got exception: " << we.what() << "\n";}
-  try{bfp->GetNamedData(time, "E:THCURR",&THCURR);}catch (WebAPIException &we) {mf::LogDebug("ICARUSBNBRetriever")<< "At time : " << time << " " << "got exception: " << we.what() << "\n";}
-  
-  //crunch the times 
-  unsigned long int time_closest_int = (int) TOR860_time;
-  double time_closest_ns = (TOR860_time - time_closest_int)*1e9;
-  
-  //Store everything in our data-product
-  sbn::BNBSpillInfo beamInfo;
-  beamInfo.TOR860 = TOR860*1e12; //add in factor of 1e12 protons to get correct POT units
-  beamInfo.TOR875 = TOR875*1e12; //add in factor of 1e12 protons to get correct POT units
-  beamInfo.LM875A = LM875A;
-  beamInfo.LM875B = LM875B;
-  beamInfo.LM875C = LM875C;
-  beamInfo.HP875 = HP875;
-  beamInfo.VP875 = VP875;
-  beamInfo.HPTG1 = HPTG1;
-  beamInfo.VPTG1 = VPTG1;
-  beamInfo.HPTG2 = HPTG2;
-  beamInfo.VPTG2 = VPTG2;
-  beamInfo.BTJT2 = BTJT2;
-  beamInfo.THCURR = THCURR;
-  beamInfo.spill_time_s = time_closest_int;
-  beamInfo.spill_time_ns = time_closest_ns;    
-
-  for(auto const& MWRdata: unpacked_MWR){
-    std::ignore = MWRdata;
-    assert(!MWRdata.empty());
-  }
-
-  if(unpacked_MWR[0].empty()){
-    beamInfo.M875BB.clear();
-    beamInfo.M875BB_spill_time_diff = -999;//units in seconds
-  }
-  else{
-    beamInfo.M875BB = unpacked_MWR[0][matched_MWR[0]];
-    beamInfo.M875BB_spill_time_diff = (MWR_times[0][matched_MWR[0]] - time);
-  }
-
- if(unpacked_MWR[1].empty()){
-    beamInfo.M876BB.clear();
-    beamInfo.M876BB_spill_time_diff = -999;//units in seconds
- }
- else{
-   beamInfo.M876BB = unpacked_MWR[1][matched_MWR[1]];
-   beamInfo.M876BB_spill_time_diff = (MWR_times[1][matched_MWR[1]] - time);
- }
-
- if(unpacked_MWR[2].empty()){
-    beamInfo.MMBTBB.clear();
-    beamInfo.MMBTBB_spill_time_diff = -999;//units in seconds
-  }
- else{
-   beamInfo.MMBTBB = unpacked_MWR[2][matched_MWR[2]];
-   beamInfo.MMBTBB_spill_time_diff = (MWR_times[2][matched_MWR[2]] - time);
- }
-  // We do not write these to the art::Events because 
-  // we can filter events but want to keep all the POT 
-  // information, so we'll write it to the SubRun
-  
-  beamInfo.event = eventID.event(); // the rest of ID is known by art::SubRun
-  
-  return beamInfo;
-}
-
 
 void sbn::ICARUSBNBRetriever::beginSubRun(art::SubRun& sr)
 {
