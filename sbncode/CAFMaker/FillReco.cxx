4223d256947abf469c7c3d5c6a687c61926d81cd
Anna Heggestuen <anna.heggestuen@colostate.edu>
2025-06-02 19:06:33 -0500

    within FillICARUSOpFlash, reconstruct the flash time w.r.t. RWM time using information stored in PMTBeamSignal

--- sbncode/CAFMaker/FillReco.cxx
+++ sbncode/CAFMaker/FillReco.cxx
@@ -183,6 +183,7 @@ namespace caf
   void FillICARUSOpFlash(const recob::OpFlash &flash,
                   std::vector<recob::OpHit const*> const& hits,
                   int cryo, 
+                  std::vector<sbn::timing::PMTBeamSignal> RWMTimes,
                   caf::SROpFlash &srflash,
                   bool allowEmpty) {
 
@@ -192,11 +193,16 @@ namespace caf
     srflash.timewidth = flash.TimeWidth();
 
     double firstTime = std::numeric_limits<double>::max();
+    std::map<int, double> startmap, risemap;
     for(const auto& hit: hits){
       double const hitTime = hit->HasStartTime()? hit->StartTime(): hit->PeakTime();
       if (firstTime > hitTime)
         firstTime = hitTime;
+      if (!RWMTimes.empty())
+        sbn::timing::SelectFirstOpHitByTime(hit,startmap,risemap); 
     }
+    srflash.rwmtime = getFlashBunchTime(risemap, RWMTimes);
+    std::cout << "Flash at Time " << flash.Time() << " has " << startmap.size() << " opHit channels, flashTime_rwm = " <<  srflash.rwmtime << " \n";
     srflash.firsttime = firstTime;
 
     srflash.cryo = cryo; // 0 in SBND, 0/1 for E/W in ICARUS
