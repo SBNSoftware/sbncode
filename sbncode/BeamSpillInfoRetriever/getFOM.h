08066a940c7ea9ee6542227471cb5d0b8a16d77b
Max Dubnowski <90648380+maxdubnowski@users.noreply.github.com>
2025-08-07 12:21:01 -0400

    Update getFOM.h

--- sbncode/BeamSpillInfoRetriever/getFOM.h
+++ sbncode/BeamSpillInfoRetriever/getFOM.h
@@ -1,5 +1,10 @@
 #ifndef _GETFOM_H
 #define _GETFOM_H
+/**
+ * @file   sbncode/BeamSpillInfoRetriever/getFOM.h
+ * @brief  Beam quality figures of merit.
+ * @author Max Dubnowski (maxdub@upenn.sas.edu)
+ */
 
 #include "sbnobj/Common/POTAccounting/BNBSpillInfo.h"
 
@@ -7,19 +12,35 @@
 
 namespace sbn
 {
-  //typedef std::vector<bnb::bnbAutoTune> autoTunes;
-  //float getFOM2(std::string beam, const gov::fnal::uboone::datatypes::ub_BeamHeader& bh, const std::vector<gov::fnal::uboone::datatypes::ub_BeamData>& bd, const bnb::bnbAutoTune settings = bnb::bnbAutoTune(), bool useAutoTune=true);
+  /**
+   * @brief Returns a Figure of Merit on BNB beam quality.
+   *
+   * The figure of merit is described in [SBN DocDB 41901](https://sbn-docdb.fnal.gov/cgi-bin/sso/ShowDocument?docid=41901).
+   * Inputs the BNBSpillInfo and returns the BNB Quality Metric called FOM, derived from MicroBooNE's FOM
+   */
   float getFOM(BNBSpillInfo& spill);
 
-  /* autoTunes cacheAutoTuneHistory(); */
-  /* bnb::bnbAutoTune getSettings(const autoTunes& history, const gov::fnal::uboone::datatypes::ub_BeamHeader& bh); */
-  /* bnb::bnbAutoTune getSettings(const autoTunes& history, const raw::BeamInfo& bi); */
-  /* bnb::bnbAutoTune getSettings(const autoTunes& history, const uint64_t utctstamp); */
+  /**
+    * @brief Inside the getFOM script, takes the positions and angles of the beam and calculates the BNB FOM
+    */
   double calcFOM(double horpos,double horang,double verpos,double verang,double tor,double tgtsx,double tgtsy);
+    
+  /**  
+    * @brief Takes in the centroid and sigma of the beam, along with transfer matrices, and will determine the beam's 
+    * 2D gaussian position depending where on the target is being measured. The code "swims" up the target to calculate these
+    */
   void swimBNB(const double centroid1[6], const double sigma1[6][6], 
                const double xferc[6][6], const double xfers[6][6],
                double &cx, double& cy, double &sx, double &sy, double &rho);
+ 
+  /**
+    * @brief Integrates the 2D modelled gaussian beam overlapping with the target, and returns the fraction outside the beam 
+    */
   double func_intbivar(const double cx, const double cy, const double sx, const double sy, const double rho );
+ 
+  /**
+    * @brief Inputs the MWR Data and determines the centroid, sigma, and chi2 value of a gaussian fit of the beam
+    */
   void processBNBprofile(const double* mwdata, double &x, double& sx, double& chi2); 
   
 }
