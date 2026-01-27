b34ca44e9435e77260a79be6c0999f0a3be43087
VCLanNguyen <74775793+VCLanNguyen@users.noreply.github.com>
2026-01-27 12:09:31 -0800

    Change flux type from kSimple_Flux to kNtuple

--- sbncode/EventGenerator/MeVPrtl/Tools/BNBKaonGen_tool.cc
+++ sbncode/EventGenerator/MeVPrtl/Tools/BNBKaonGen_tool.cc
@@ -311,7 +311,7 @@ simb::MCFlux BNBKaonGen::MakeMCFlux(const bsim::BooNe &boone) {
                                       << std::endl;
   }
 
-  flux.fFluxType = simb::kSimple_Flux;
+  flux.fFluxType = simb::kNtuple;
   flux.fnimpwt   = fBooneNtp.beamwgt;
   flux.fvx       = fBooneNtp.ini_pos[0][0]; //0
   flux.fvy       = fBooneNtp.ini_pos[0][1]; //0
