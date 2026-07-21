e109071434cbac35fed99810942138df976d62b8
Alejandro Sánchez Castillo <asanchezcastillo@ugr.es>
2026-07-21 09:43:30 -0500

    Add propagation time information on corrected opflash

--- sbncode/CAFMaker/FillReco.cxx
+++ sbncode/CAFMaker/FillReco.cxx
@@ -372,6 +372,8 @@ namespace caf
       slice.correctedOpFlash.NuToFLight  = _correctedOpFlash.NuToFLight;
       slice.correctedOpFlash.NuToFCharge  = _correctedOpFlash.NuToFCharge;
       slice.correctedOpFlash.OpFlashT0Corrected  = _correctedOpFlash.OpFlashT0Corrected;
+      slice.correctedOpFlash.ParticlePropagationTime = _correctedOpFlash.ParticlePropagationTime;
+      slice.correctedOpFlash.PhotonPropagationTime = _correctedOpFlash.PhotonPropagationTime;
     }
   }
 
